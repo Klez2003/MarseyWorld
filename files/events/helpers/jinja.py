90f023cff57d7482c5976c62054c2810227763d8
geese_suck <deandre.williams@geese-suck.us>
2022-11-29 13:11:58 -0800

    jingle jangle

--- files/events/helpers/jinja.py
+++ files/events/helpers/jinja.py
@@ -1,7 +1,7 @@
 EVENT_JINJA_CONST = {
 	"EVENT_BANNER": "banner_rDrama.html",
 	"EVENT_SIDEBAR": True,
-	"EVENT_STYLES": "spooky.css",
+	"EVENT_STYLES": "blizzard.css",
 	"EVENT_AWARDS": True,
 	"EVENT_MUSIC": "music.html",
 }
