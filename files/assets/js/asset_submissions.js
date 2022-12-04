0773575c4b8702ac5bbc978b507374c1b708e0c3
justcool393 <justcool393@gmail.com>
2022-12-04 13:26:56 -0600

    asset submissions js page

---
+++ files/assets/js/asset_submissions.js
@@ -0,0 +1,83 @@
+document.onpaste = function(event) {
+    files = structuredClone(event.clipboardData.files);
+
+    filename = files[0]
+
+    if (filename)
+    {
+        filename = filename.name.toLowerCase()
+        f=document.getElementById('file-upload');
+        f.files = files;
+        document.getElementById('filename-show').textContent = filename;
+        if (IMAGE_FORMATS.some(s => filename.endsWith(s)))
+        {
+            var fileReader = new FileReader();
+            fileReader.readAsDataURL(f.files[0]);
+            fileReader.addEventListener("load", function () {
+                document.getElementById('image-preview').setAttribute('src', this.result);
+                document.getElementById('image-preview').classList.remove('d-none');
+            });
+        }
+    }
+}
+
+document.getElementById('file-upload').addEventListener('change', function(){
+    f=document.getElementById('file-upload');
+    document.getElementById('filename-show').textContent = document.getElementById('file-upload').files[0].name.substr(0, 20);
+    filename = f.files[0].name.toLowerCase()
+    if (IMAGE_FORMATS.some(s => filename.endsWith(s)))
+    {
+        var fileReader = new FileReader();
+        fileReader.readAsDataURL(f.files[0]);
+        fileReader.addEventListener("load", function () {
+            document.getElementById('image-preview').setAttribute('src', this.result);
+            document.getElementById('image-preview').classList.remove('d-none');
+        });
+        document.getElementById('submit-hat').disabled = false;
+    }
+})
+
+function approveMarsey(t, name) {
+    postToast(t, `/admin/approve/marsey/${name}`,
+        {
+            "tags": document.getElementById(`${name}-tags`).value,
+            "name": document.getElementById(`${name}-name`).value,
+        },
+        () => {
+            document.getElementById(`${name}-marsey`).remove()
+        }
+    );
+}
+
+function removeMarsey(t, name) {
+    postToast(t, `/remove/marsey/${name}`,
+        {
+        },
+        () => {
+            document.getElementById(`${name}-marsey`).remove()
+        }
+    );
+}
+
+function approveHat(t, name) {
+    postToast(t, `/admin/approve/hat/${name}`,
+        {
+            "description": document.getElementById(`${name}-description`).value,
+            "name": document.getElementById(`${name}-name`).value,
+            "price": document.getElementById(`${name}-price`).value,
+        },
+        () => {
+            document.getElementById(`${name}-hat`).remove()
+        }
+    );
+}
+
+function removeHat(t, name) {
+    postToast(t, `/remove/hat/${name}`,
+        {
+        },
+        () => {
+            document.getElementById(`${name}-hat`).remove()
+        }
+    );
+}
