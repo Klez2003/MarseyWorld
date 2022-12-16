bd4254653d0e5c338731e2715d0c4af480a106e1
justcool393 <justcool393@gmail.com>
2022-12-16 14:16:36 -0600

    gingerbread award

--- files/events/helpers/const.py
+++ files/events/helpers/const.py
@@ -1,4 +1,53 @@
 EVENT_AWARDS = {
-
+    "snow": {
+		"kind": "snow",
+		"title": "Snow",
+		"description": "",
+		"icon": "fas fa-snowflake",
+		"color": "text-blue-200",
+		"price": 300,
+		"deflectable": False,
+		"cosmetic": True
+	},
+	"gingerbread": {
+		"kind": "gingerbread",
+		"title": "Gingerbread",
+		"description": "",
+		"icon": "fas fa-gingerbread-man",
+		"color": "",
+		"price": 300,
+		"deflectable": False,
+		"cosmetic": True
+	},
+	"lights": {
+		"kind": "lights",
+		"title": "Lights",
+		"description": "",
+		"icon": "fas fa-lights-holiday",
+		"color": "",
+		"price": 300,
+		"deflectable": False,
+		"cosmetic": True
+	},
+	"candycane": {
+		"kind": "candycane",
+		"title": "Candy Cane",
+		"description": "",
+		"icon": "fas fa-candy-cane",
+		"color": "",
+		"price": 400,
+		"deflectable": False,
+		"cosmetic": True
+	},
+	"fireplace": {
+		"kind": "fireplace",
+		"title": "Fireplace",
+		"description": "",
+		"icon": "fas fa-fireplace",
+		"color": "",
+		"price": 600,
+		"deflectable": False,
+		"cosmetic": True
+	},
 }
 EVENT_FORCED_HAT = '/i/hats/Santa Hat I.webp?h=7'
