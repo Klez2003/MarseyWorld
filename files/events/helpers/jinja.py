b7f07ff53a18d28826f0f0e351a166dd63832ed3
justcool393 <justcool393@gmail.com>
2022-12-15 20:34:53 -0600

    header icon: add event extension point

--- files/events/helpers/jinja.py
+++ files/events/helpers/jinja.py
@@ -4,12 +4,6 @@ EVENT_JINJA_CONST = {
 	"EVENT_STYLES": "blizzard.css",
 	"EVENT_AWARDS": True,
 	"EVENT_MUSIC": "music.html",
-	"EVENT_HEADER_ICONS": [
-		"/e/marseypartyxmas.webp?v=1",
-		"/e/marseysanta3.webp?v=1",
-		"/e/marseychristmaself2.webp?v=1",
-		"/e/marseysnowman.webp",
-	],
 	"EVENT_VISITORS_HERE_FLAVOR": [
 		' santa enjoyers kissing under a misletoe',
 		' bringing up family drama at Christmas dinner',
