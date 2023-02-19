6aa361a2e7b0499f70c222994d33a57c401c3ff1
Aevann <randomname42029@gmail.com>
2023-02-19 22:26:36 +0200

    minor fix to the migration sql command

--- migrations/20230219-message-search.sql
+++ migrations/20230219-message-search.sql
@@ -1,1 +1,1 @@
-update comments set body=body_html where body is null and sentto is not null and parent_submission is null;
+update comments set body=body_html where body is null and sentto is not null and parent_submission is null and length(body_html) < 10001;
