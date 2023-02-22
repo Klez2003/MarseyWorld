e5b9009dc27943ce031d68cbc76b0beb355af98a
Aevann <randomname42029@gmail.com>
2023-02-22 19:00:38 +0200

    notify users when their flair gets changed by jannies

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -846,6 +846,13 @@ def admin_title_change(user_id, v):
 		)
 	g.db.add(ma)
 
+	if user.flairchanged:
+		message = f"@{v.username} (a site admin) has locked your flair to `{user.customtitleplain}`."
+	else:
+		message = f"@{v.username} (a site admin) has changed your flair to `{user.customtitleplain}`. You can change it back in the settings."
+		
+	send_repeatable_notification(user.id, message)
+
 	return {"message": f"@{user.username}'s flair has been changed!"}
 
 @app.post("/ban_user/<id>")
