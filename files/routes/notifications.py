6cc9c784cccf1ef4f2e7093841f83b1c1f4d7bc2
Aevann <randomname42029@gmail.com>
2023-02-26 01:44:31 +0200

    minor changes to notifs

--- files/routes/notifications.py
+++ files/routes/notifications.py
@@ -307,7 +307,7 @@ def notifications(v:User):
 	cids = [x[0].id for x in comments]
 
 	listing = []
-	total = []
+	total = [x[0] for x in comments]
 	for c, n in comments:
 		if n.created_utc > 1620391248: c.notif_utc = n.created_utc
 		if not n.read:
@@ -316,13 +316,16 @@ def notifications(v:User):
 			g.db.add(n)
 
 		if c.parent_submission or c.wall_user_id:
+			total.append(c)
+
 			if c.replies2 == None:
 				c.replies2 = g.db.query(Comment).filter_by(parent_comment_id=c.id).filter(or_(Comment.author_id == v.id, Comment.id.in_(cids))).order_by(Comment.id.desc()).all()
 				total.extend(c.replies2)
 				for x in c.replies2:
 					if x.replies2 == None: x.replies2 = []
+			
 			count = 0
-			while count < 50 and c.parent_comment and (count == 0 or c.parent_comment.author_id == v.id or c.parent_comment.id in cids):
+			while count < 50 and c.parent_comment and (c.parent_comment.author_id == v.id or c.parent_comment.id in cids):
 				count += 1
 				c = c.parent_comment
 				if c.replies2 == None:
@@ -336,7 +339,6 @@ def notifications(v:User):
 			while c.parent_comment:
 				c = c.parent_comment
 			c.replies2 = g.db.query(Comment).filter_by(parent_comment_id=c.id).order_by(Comment.id).all()
-			total.extend(c.replies2)
 
 		if c not in listing: listing.append(c)
 
