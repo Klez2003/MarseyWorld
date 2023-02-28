d7f43d4e3ae5da4721fc51569580023a649e052a
Aevann <randomname42029@gmail.com>
2023-03-01 00:15:05 +0200

    make it possible to delete poll options

--- files/classes/comment.py
+++ files/classes/comment.py
@@ -297,7 +297,7 @@ class Comment(Base):
 
 			if f'{s}{o.body}{s}' in body:
 				body = body.replace(f'{s}{o.body}{s}', option_body)
-			else:
+			elif o.created_utc < 1677622477:
 				body += option_body
 
 		if not self.ghost and self.author.show_sig(v):
