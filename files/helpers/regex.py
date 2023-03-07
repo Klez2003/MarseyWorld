2a9bfad610031d48b040cf8ab0582734b7b811c9
Aevann <randomname42029@gmail.com>
2023-03-07 03:40:48 +0200

    minor syntax change

--- files/helpers/regex.py
+++ files/helpers/regex.py
@@ -83,8 +83,9 @@ audio_sub_regex = re.compile(f'(<p>[^<]*)(https:\/\/([a-z0-9-]+\.)*({hosts})\/[\
 
 image_regex_extensions = '|'.join(IMAGE_FORMATS)
 image_regex = re.compile(f"(^|\s)(https:\/\/[\w\-.#&/=\?@%;+,:]{{5,250}}(\.|\?format=)({image_regex_extensions})((\?|&)[\w\-.#&/=\?@%;+,:]*)?)($|\s)", flags=re.I|re.A)
-image_regex_extensions = image_regex_extensions.replace('|gif', '')
-imgur_regex = re.compile(f'(https:\/\/i\.imgur\.com\/[a-z0-9]+)\.({image_regex_extensions})', flags=re.I|re.A)
+
+image_regex_extensions_no_gif = image_regex_extensions.replace('|gif', '')
+imgur_regex = re.compile(f'(https:\/\/i\.imgur\.com\/[a-z0-9]+)\.({image_regex_extensions_no_gif})', flags=re.I|re.A)
 
 giphy_regex = re.compile('(https:\/\/media\.giphy\.com\/media\/[a-z0-9]+\/giphy)\.gif', flags=re.I|re.A)
 
