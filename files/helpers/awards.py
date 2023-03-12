6c239faacbc52dbea6c807456456a668b12edc99
Aevann <randomname42029@gmail.com>
2023-03-12 16:27:22 +0200

    better notification when bite award expires

--- files/helpers/awards.py
+++ files/helpers/awards.py
@@ -67,7 +67,13 @@ def award_timers(v, bot=False):
 		badge = v.has_badge(167)
 	if v.bite and v.bite < now:
 		v.bite = None
-		notify_if_not_bot("Your vampire status has ended!")
+		
+		notif_text = "Your vampire status has ended."
+		if v.old_house:
+			house = v.old_house.replace(' Founder', '')
+			notif_text += f" You're now back in House {house}!"
+		notify_if_not_bot(notif_text)
+		
 		v.house = v.old_house
 		v.old_house = ''
 		badge = v.has_badge(168)
