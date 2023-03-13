95dc636aa0b805e48ed87af7d5b11eb45d196303
Aevann <randomname42029@gmail.com>
2023-03-13 07:29:50 +0200

    add .party to supported tlds

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -44,7 +44,7 @@ TLDS = ( # Original gTLDs and ccTLDs
 	# New gTLDs
 	'app','cleaning','club','dev','farm','florist','fun','gay','lgbt','life','lol',
 	'moe','mom','monster','new','news','online','pics','press','pub','site','blog',
-	'vip','win','world','wtf','xyz','video','host','art','media','wiki','tech','cooking','network',
+	'vip','win','world','wtf','xyz','video','host','art','media','wiki','tech','cooking','network','party',
 	)
 
 allowed_tags = ('b','blockquote','br','code','del','em','h1','h2','h3','h4','h5','h6','hr','i',
