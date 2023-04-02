0ce52ec78a3a679704352eb76670607ac72649c1
Aevann <randomname42029@gmail.com>
2023-04-02 08:17:15 +0200

    make "marsify" and "chud" mutually exclusive again to prevent cheap ban

--- files/routes/awards.py
+++ files/routes/awards.py
@@ -301,6 +301,9 @@ def award_thing(v, thing_type, id):
 		if author.marseyawarded:
 			abort(409, f"{safe_username} is under the effect of a conflicting award: Marsey award!")
 
+		if author.marsify:
+			abort(409, f"{safe_username} is under the effect of a conflicting award: Marsify award!")
+
 		if author.agendaposter == 1:
 			abort(409, f"{safe_username} is already chudded permanently!")
 
@@ -388,6 +391,9 @@ def award_thing(v, thing_type, id):
 		author.verified = "Verified"
 		badge_grant(user=author, badge_id=150)
 	elif kind == 'marsify':
+		if author.agendaposter:
+			abort(409, f"{safe_username} is under the effect of a conflicting award: Chud award!")
+
 		if not author.marsify or author.marsify != 1:
 			if author.marsify: author.marsify += 86400
 			else: author.marsify = int(time.time()) + 86400
