79e9d24ae25d725137088320372ab1adee95ffde
Aevann <randomname42029@gmail.com>
2023-05-03 18:46:29 +0300

    fix migration sql commands

--- migrations/20230503-add-new-patron-tier-for-benefactorcels.sql
+++ migrations/20230503-add-new-patron-tier-for-benefactorcels.sql
@@ -12,4 +12,11 @@ update badges set badge_id=58 where badge_id=28;
 update badges set badge_id=badge_id+1 where badge_id in (21,22,23,24,25,26,27);
 delete from badge_defs where id=21;
 
-update users set patron=patron+1 where id in (select user_id from badges where badge_id in (22,23,24,25,26,27,28));
+update users set patron=1 where patron>0;
+update users set patron=2 where id in (select user_id from badges where badge_id=22);
+update users set patron=3 where id in (select user_id from badges where badge_id=23);
+update users set patron=4 where id in (select user_id from badges where badge_id=24);
+update users set patron=5 where id in (select user_id from badges where badge_id=25);
+update users set patron=6 where id in (select user_id from badges where badge_id=26);
+update users set patron=7 where id in (select user_id from badges where badge_id=27);
+update users set patron=8 where id in (select user_id from badges where badge_id=28);
