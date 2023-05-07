e254a06d77926248031136575c6f7fe7d72c3ecb
Aevann <randomname42029@gmail.com>
2023-05-07 22:09:23 +0300

    add "saves" to COMMENT_SORTS

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -401,7 +401,7 @@ PIN_AWARD_TEXT = " (pin award)"
 
 THEMES = ["4chan","classic","classic_dark","coffee","dark","dramblr","light","midnight","tron","win98"]
 BACKGROUND_CATEGORIES = ["glitter", "anime", "fantasy", "solarpunk", "pixelart"]
-COMMENT_SORTS = ["hot", "new", "old", "top", "bottom", "controversial"]
+COMMENT_SORTS = ["hot", "new", "old", "top", "bottom", "controversial", "saves"]
 SORTS = COMMENT_SORTS + ["bump", "comments", "views", "subscriptions", "saves"]
 TIME_FILTERS = ["hour", "day", "week", "month", "year", "all"]
 PAGE_SIZES = (10, 25, 50, 100)
