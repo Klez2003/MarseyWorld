e978189226d13ef2ede9c57043b0c0e1789334bb
Aevann <randomname42029@gmail.com>
2023-06-06 23:44:37 +0300

    remove commented migration code

--- files/cli.py
+++ files/cli.py
@@ -8,10 +8,3 @@ os.chdir(Path(__file__).parent.parent)
 from .__main__ import app, db_session, cache
 from flask import g
 import files.helpers.cron
-
-#from flask_migrate import Migrate
-#from flask_sqlalchemy import SQLAlchemy
-#import files.classes
-
-#db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
