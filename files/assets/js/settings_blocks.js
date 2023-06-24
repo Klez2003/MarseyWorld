c097a7c244c0f23dcd7522571f826e715b192b4e
Aevann <randomname42029@gmail.com>
2023-06-24 20:36:27 +0300

    fix error msgs in /settings/blocks

--- files/assets/js/settings_blocks.js
+++ files/assets/js/settings_blocks.js
@@ -1,23 +1,9 @@
-function block_user() {
+function block_user(t) {
 	const usernameField = document.getElementById("block-username");
 	const isValidUsername = usernameField.checkValidity();
 	username = usernameField.value;
 	if (isValidUsername) {
-		const xhr = new XMLHttpRequest();
-		xhr.open("post", "/settings/block");
-		xhr.setRequestHeader('xhr', 'xhr');
-		f=new FormData();
-		f.append("username", username);
-		f.append("formkey", formkey());
-		xhr.onload=function(){
-			if (xhr.status<300) {
-				location.reload();
-			}
-			else {
-				showToast(false, "Error, please try again later.");
-			}
-		}
-		xhr.send(f)
+		postToastReload(t,`/settings/block?username=${username}`);
 	}
 }
 
