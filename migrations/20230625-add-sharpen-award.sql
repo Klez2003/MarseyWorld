c2ecf2296a8f3d7832aebf293236e459c2240624
Aevann <randomname42029@gmail.com>
2023-06-26 17:45:09 +0300

    fix missing sql commands in last commit

--- migrations/20230625-add-sharpen-award.sql
+++ migrations/20230625-add-sharpen-award.sql
@@ -1,1 +1,3 @@
-alter table users add column sharpen integer;
+alter table users add column sharpen integer default 0;
+alter table users alter column sharpen drop default;
+CREATE INDEX users_edgified_idx ON public.users USING btree (sharpen);
