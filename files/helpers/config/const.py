2494b78116e12e5b4eaf9d472a4fe848a5878ca6
Aevann <randomname42029@gmail.com>
2023-06-28 00:32:34 +0300

    increase COSMETIC_AWARD_COIN_AWARD_PCT from 10% to 50%

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -654,7 +654,7 @@ SUB_BANNER_LIMIT = 10
 BIO_FRIENDS_ENEMIES_LENGTH_LIMIT = 5000 # do not make larger than 5000 characters without altering the table
 BIO_FRIENDS_ENEMIES_HTML_LENGTH_LIMIT = 20000 # do not make larger than 20000 characters without altering the table
 
-COSMETIC_AWARD_COIN_AWARD_PCT = 0.10
+COSMETIC_AWARD_COIN_AWARD_PCT = 0.50
 
 TRUESCORE_CHAT_MINIMUM = 0
 TRUESCORE_GHOST_MINIMUM = 0
