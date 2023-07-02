4aedec05d1552a42f587096093896c30e563578b
Aevann <randomname42029@gmail.com>
2023-07-03 00:21:35 +0300

    increase session age

--- files/__main__.py
+++ files/__main__.py
@@ -44,7 +44,7 @@ if not IS_LOCALHOST:
 app.config["SESSION_COOKIE_NAME"] = "session_" + environ.get("SITE_NAME").strip().lower()
 app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024 if SITE == 'watchpeopledie.tv' else 100 * 1024 * 1024
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-app.config["PERMANENT_SESSION_LIFETIME"] = SESSION_LIFETIME
+app.config["PERMANENT_SESSION_LIFETIME"] = 60 * 60 * 24 * 999
 app.config['SESSION_REFRESH_EACH_REQUEST'] = False
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
