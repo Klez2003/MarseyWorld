f705a1c205bf9f5fe7feb5e4055fc0600de6e82f
Aevann <randomname42029@gmail.com>
2023-07-14 14:23:36 +0300

    like last commit

--- files/assets/js/bugs.js
+++ files/assets/js/bugs.js
@@ -3,7 +3,7 @@ const shit_num = document.getElementById('shit_num').value
 new BugController({
 	imageSprite: "/i/fly-sprite.webp",
 	canDie: false,
-	minBugs: Math.min(MINFLIES + (shit_num * 2), 50),
-	maxBugs: Math.min(MAXFLIES + (shit_num * 2), 100),
+	minBugs: Math.min(MINFLIES + (shit_num * 2), 150),
+	maxBugs: Math.min(MAXFLIES + (shit_num * 2), 150),
 	mouseOver: "multiply"
 });
