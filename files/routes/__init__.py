2413af3467574d0b742a288a558105e946ab471d
Aevann <randomname42029@gmail.com>
2023-07-14 12:22:08 +0300

    remove worldcup shit

--- files/routes/__init__.py
+++ files/routes/__init__.py
@@ -46,7 +46,6 @@ if FEATURES['HATS']:
 	from .hats import *
 if FEATURES['ASSET_SUBMISSIONS']:
 	from .asset_submissions import *
-from .special import *
 from .push_notifs import *
 if FEATURES['PING_GROUPS']:
 	from .groups import *
