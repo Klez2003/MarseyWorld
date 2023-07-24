a8976f0e113b986fa661cee3287a7963c4aa8fac
Aevann <randomname42029@gmail.com>
2023-07-24 13:20:08 +0300

    fix cron.sh

--- cron.sh
+++ cron.sh
@@ -2,7 +2,7 @@
 
 LOG='/var/log/rdrama/cron.log'
 
-echo -e "\n"======================================== $1 === $(date --iso-8601=minutes)" >> "$LOG"
+echo -e "\n======================================== $1 === $(date --iso-8601=minutes)" >> "$LOG"
 
 source /e
 /usr/local/bin/flask cron $1 >>"$LOG" 2>&1
