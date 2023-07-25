b30c37cd99f42b2fc318b7dfd8391bd0e772c51d
Aevann <randomname42029@gmail.com>
2023-07-26 01:37:54 +0300

    add Random sort

--- files/helpers/sorting_and_time.py
+++ files/helpers/sorting_and_time.py
@@ -53,6 +53,8 @@ def sort_objects(sort, objects, cls):
 		return objects.order_by((cls.upvotes+1)/(cls.downvotes+1) + (cls.downvotes+1)/(cls.upvotes+1), cls.downvotes.desc(), cls.created_utc.desc())
 	elif sort == "bottom":
 		return objects.order_by(cls.upvotes - cls.downvotes, cls.created_utc.desc())
+	elif sort == "random":
+		return objects.order_by(func.random())
 	else:
 		return objects.order_by(cls.downvotes - cls.upvotes, cls.created_utc.desc())
 
