aa7426bbde412705e0a49576e572425b88b9c339
Aevann <randomname42029@gmail.com>
2023-07-27 03:12:50 +0300

    consistency

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -5,6 +5,6 @@ LOG='/var/log/rdrama/cron.log'
 echo -e "\n======================================== $1 === $(date --iso-8601=minutes)" >> "$LOG"
 
 source /e
-/usr/local/bin/flask cron $1 >>"$LOG" 2>&1
+/usr/local/bin/flask cron $1 >> "$LOG" 2>&1
 
 echo -e "======================================== $1 === $(date --iso-8601=minutes)" >> "$LOG"
