731dafcfbd9ca974fb55c62740b96902702a914d
Aevann <randomname42029@gmail.com>
2023-07-30 02:40:26 +0300

    remove leftover code

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -615,7 +615,7 @@ def allowed_attributes_emojis(tag, name, value):
 			if value.startswith(f'{SITE_FULL_IMAGES}/') : return True
 		if name == 'loading' and value == 'lazy': return True
 		if name == 'data-bs-toggle' and value == 'tooltip': return True
-		if name in {'g','glow','t'} and not value: return True
+		if name in {'g','glow'} and not value: return True
 		if name in {'alt','title'}: return True
 
 	if tag == 'span':
