fbfb2dc8ed9c473b0c86b80ce4f1f3df022a3aa9
Aevann <randomname42029@gmail.com>
2023-08-04 00:04:09 +0300

    fix migration

--- migrations/20230801-move-lifetimedonated-to-column.sql
+++ migrations/20230801-move-lifetimedonated-to-column.sql
@@ -1,3 +1,4 @@
+drop function lifetimedonated;
 alter table users add column lifetimedonated int not null default 0;
 alter table users alter column lifetimedonated drop default;
 update users set lifetimedonated=(select sum(amount) from transactions where transactions.email = users.email) where (select sum(amount) from transactions where transactions.email = users.email)>0;
