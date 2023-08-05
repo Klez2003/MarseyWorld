3df7fb98d8813d0013c036cf074defb001473d0e
Aevann <randomname42029@gmail.com>
2023-08-05 21:59:11 +0300

    fix 500 error

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -4,7 +4,7 @@ import re
 import signal
 from functools import partial
 from os import path, listdir
-from urllib.parse import parse_qs, urlparse, unquote, ParseResult
+from urllib.parse import parse_qs, urlparse, unquote, ParseResult, urlencode
 
 from sqlalchemy.sql import func
 
