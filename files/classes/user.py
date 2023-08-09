132056d61b1eb9c966ed5ef926cd3887f555c172
Aevann <randomname42029@gmail.com>
2023-08-09 21:45:25 +0300

    exempt autist from earning coins

--- files/classes/user.py
+++ files/classes/user.py
@@ -202,6 +202,9 @@ class User(Base):
 		if SITE == 'watchpeopledie.tv' and self.id == 5222:
 			return
 
+		if SITE == 'rdrama.net' and self.id == 5237:
+			return
+
 		user_query = g.db.query(User).options(load_only(User.id)).filter_by(id=self.id)
 
 		if currency == 'coins':
