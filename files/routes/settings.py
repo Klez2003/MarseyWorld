0633c9b71d6585bd586e12a1928c39a5eb65d2fc
Aevann <randomname42029@gmail.com>
2023-08-10 17:00:54 +0300

    fix bug caused by 4602e374eeaee91ffebab01fef4715dcb8574139

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -926,7 +926,7 @@ def settings_title_change(v):
 	if v.flairchanged: abort(403)
 
 	processed = process_settings_plaintext("title", v.customtitleplain, 100)
-	if isinstance(processed, tuple):
+	if not isinstance(processed, str):
 		return processed
 
 	customtitle = filter_emojis_only(processed)
@@ -951,7 +951,7 @@ def settings_title_change(v):
 @auth_required
 def settings_pronouns_change(v):
 	processed = process_settings_plaintext("pronouns", v.pronouns, 15)
-	if isinstance(processed, tuple):
+	if not isinstance(processed, str):
 		return processed
 
 	pronouns = processed
@@ -979,7 +979,7 @@ def settings_checkmark_text(v):
 		abort(403, "You don't have a checkmark to edit its hover text!")
 
 	processed = process_settings_plaintext("checkmark-text", v.verified, 100)
-	if isinstance(processed, tuple):
+	if not isinstance(processed, str):
 		return processed
 
 	v.verified = processed
