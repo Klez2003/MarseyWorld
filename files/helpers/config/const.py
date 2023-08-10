ec3079524b21ee28384c1393401c967eee31f32b
Aevann <randomname42029@gmail.com>
2023-08-10 15:07:51 +0300

    Revert "fix "cool down" and "cool off" becoming "fetch down" and "fetch off""
    This reverts commit 821557356165694551bbe49c200cce5edf29c77d.

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -232,6 +232,7 @@ if SITE_NAME == 'rDrama':
 		"its almost as if": "I'm a retard but",
 		"its almost like": "I'm a retard but",
 		"my brother in christ": "my brother in Allah (ﷻ)",
+		"(?<![a-z])cool (?!it)": "fetch ",
 		"krayon": "krayon (sister toucher)",
 		"discord": "groomercord",
 		"allah(?! \()": "Allah (ﷻ)",
