Klez2003/MarseyWorld

commit 3ec2038caf16d17dd35f3fcbd3b089bb765e8aab

same as last commit

files/classes/user.py
@@ -414,11 +414,11 @@ class User(Base):
 	@property
 	@lazy
 	def sub_blocks(self):
-		stealth = set([x[0] for x in g.db.query(Sub.name).filter_by(stealth=True).all()])
-		stealth = stealth - set([x[0] for x in g.db.query(SubJoin.sub).filter_by(user_id=self.id).all()])
+		stealth = set([x[0] for x in g.db.query(Sub.name).filter_by(stealth=True)])
+		stealth = stealth - set([x[0] for x in g.db.query(SubJoin.sub).filter_by(user_id=self.id)])
 		if self.chud == 1: stealth = stealth - {'chudrama'}
 
-		return list(stealth) + [x[0] for x in g.db.query(SubBlock.sub).filter_by(user_id=self.id).all()]
+		return list(stealth) + [x[0] for x in g.db.query(SubBlock.sub).filter_by(user_id=self.id)]
 
 	@lazy
 	def blocks(self, sub):
@@ -431,7 +431,7 @@ class User(Base):
 	@property
 	@lazy
 	def all_follows(self):
-		return [x[0] for x in g.db.query(SubSubscription.sub).filter_by(user_id=self.id).all()]
+		return [x[0] for x in g.db.query(SubSubscription.sub).filter_by(user_id=self.id)]
 
 	@lazy
 	def follows(self, sub):
@@ -560,7 +560,7 @@ class User(Base):
 	@lazy
 	def all_twoway_blocks(self):
 		return set([x[0] for x in g.db.query(UserBlock.target_id).filter_by(user_id=self.id).all() + \
-			g.db.query(UserBlock.user_id).filter_by(target_id=self.id).all()])
+			g.db.query(UserBlock.user_id).filter_by(target_id=self.id)])
 
 
 	def validate_2fa(self, token):
@@ -697,12 +697,12 @@ class User(Base):
 	@property
 	@lazy
 	def followed_users(self):
-		return [x[0] for x in g.db.query(Follow.target_id).filter_by(user_id=self.id).all()]
+		return [x[0] for x in g.db.query(Follow.target_id).filter_by(user_id=self.id)]
 
 	@property
 	@lazy
 	def followed_subs(self):
-		return [x[0] for x in g.db.query(SubSubscription.sub).filter_by(user_id=self.id).all()]
+		return [x[0] for x in g.db.query(SubSubscription.sub).filter_by(user_id=self.id)]
 
 	@property
 	@lazy
@@ -859,7 +859,7 @@ class User(Base):
 	@property
 	@lazy
 	def moderated_subs(self):
-		return [x[0] for x in g.db.query(Mod.sub).filter_by(user_id=self.id).order_by(Mod.sub).all()]
+		return [x[0] for x in g.db.query(Mod.sub).filter_by(user_id=self.id).order_by(Mod.sub)]
 
 	@property
 	@lazy
@@ -867,7 +867,7 @@ class User(Base):
 		return [x[0] for x in g.db.query(GroupMembership.group_name).filter(
 				GroupMembership.user_id == self.id, 
 				GroupMembership.approved_utc != None,
-			).order_by(GroupMembership.group_name).all()]
+			).order_by(GroupMembership.group_name)]
 
 	@lazy
 	def has_follower(self, user):
@@ -1016,7 +1016,7 @@ class User(Base):
 	@property
 	@lazy
 	def userblocks(self):
-		return [x[0] for x in g.db.query(UserBlock.target_id).filter_by(user_id=self.id).all()]
+		return [x[0] for x in g.db.query(UserBlock.target_id).filter_by(user_id=self.id)]
 
 	def get_relationship_count(self, relationship_cls):
 		# TODO: deduplicate (see routes/users.py)

files/classes/clients.py
@@ -44,7 +44,7 @@ class OauthApp(Base):
 
 		items = items.order_by(cls.created_utc.desc())
 		items = items.offset(100*(page-1)).limit(100)
-		items = [x.id for x in items.all()]
+		items = [x.id for x in items]
 
 		return items, total
 

files/helpers/actions.py
@@ -508,7 +508,7 @@ def execute_under_siege(v, target, body, kind):
 
 	if v.age > threshold: return
 
-	unshadowbannedcels = [x[0] for x in g.db.query(ModAction.target_user_id).filter_by(kind='unshadowban').all()]
+	unshadowbannedcels = [x[0] for x in g.db.query(ModAction.target_user_id).filter_by(kind='unshadowban')]
 	if v.id in unshadowbannedcels: return
 
 	check_for_alts(v)

files/helpers/alerts.py
@@ -138,7 +138,7 @@ def NOTIFY_USERS(text, v, oldtext=None, ghost=False, log_cost=None):
 	notify_users.update(user_ids)
 
 	if SITE_NAME == "WPD" and 'daisy' in text:
-		admin_ids = [x[0] for x in g.db.query(User.id).filter(User.admin_level >= PERMS['NOTIFICATIONS_SPECIFIC_WPD_COMMENTS']).all()]
+		admin_ids = [x[0] for x in g.db.query(User.id).filter(User.admin_level >= PERMS['NOTIFICATIONS_SPECIFIC_WPD_COMMENTS'])]
 		notify_users.update(admin_ids)
 
 
@@ -163,7 +163,7 @@ def NOTIFY_USERS(text, v, oldtext=None, ghost=False, log_cost=None):
 				return 'everyone'
 			elif i.group(1) == 'jannies':
 				group = None
-				member_ids = set([x[0] for x in g.db.query(User.id).filter(User.admin_level > 0, User.id != AEVANN_ID).all()])
+				member_ids = set([x[0] for x in g.db.query(User.id).filter(User.admin_level > 0, User.id != AEVANN_ID)])
 				coin_receivers.update(member_ids)
 			else:
 				group = g.db.get(Group, i.group(1))

files/helpers/const_stateful.py
@@ -16,7 +16,7 @@ def const_initialize():
 
 	db = db_session()
 
-	marseys_const = [x[0] for x in db.query(Emoji.name).filter(Emoji.kind=="Marsey", Emoji.submitter_id==None, Emoji.name!='chudsey').all()]
+	marseys_const = [x[0] for x in db.query(Emoji.name).filter(Emoji.kind=="Marsey", Emoji.submitter_id==None, Emoji.name!='chudsey')]
 	marseys_const2 = marseys_const + ['chudsey','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','exclamationpoint','period','questionmark']
 	marseys = db.query(Emoji).filter(Emoji.kind=="Marsey", Emoji.submitter_id==None).all()
 	for marsey in marseys:
@@ -30,7 +30,7 @@ def const_initialize():
 		SNAPPY_KONGS = db.query(Emoji.name).filter(Emoji.kind=="Donkey Kong", Emoji.submitter_id==None).all()
 		SNAPPY_KONGS = [f':#{x[0]}:' for x in SNAPPY_KONGS]
 
-	STEALTH_HOLES = [x[0] for x in db.query(Sub.name).filter_by(stealth=True).all()]
+	STEALTH_HOLES = [x[0] for x in db.query(Sub.name).filter_by(stealth=True)]
 
 	db.commit()
 	db.close()

files/helpers/cron.py
@@ -77,13 +77,13 @@ def _sub_inactive_purge_task():
 	active_holes = [x[0] for x in g.db.query(Post.sub).distinct() \
 		.filter(Post.sub != None, Post.created_utc > one_week_ago,
 			Post.private == False, Post.is_banned == False,
-			Post.deleted_utc == 0).all()]
+			Post.deleted_utc == 0)]
 	active_holes.extend(['changelog','countryclub','museumofrdrama','highrollerclub']) # holes immune from deletion
 
 	dead_holes = g.db.query(Sub).filter(Sub.name.notin_(active_holes)).all()
 	names = [x.name for x in dead_holes]
 
-	admins = [x[0] for x in g.db.query(User.id).filter(User.admin_level >= PERMS['NOTIFICATIONS_HOLE_INACTIVITY_DELETION']).all()]
+	admins = [x[0] for x in g.db.query(User.id).filter(User.admin_level >= PERMS['NOTIFICATIONS_HOLE_INACTIVITY_DELETION'])]
 
 	mods = g.db.query(Mod).filter(Mod.sub.in_(names)).all()
 	for x in mods:

files/helpers/sanitize.py
@@ -586,7 +586,7 @@ def sanitize(sanitized, golden=True, limit_pings=0, showmore=False, count_emojis
 	if g.v and g.v.admin_level >= PERMS["IGNORE_DOMAIN_BAN"]:
 		banned_domains = []
 	else:
-		banned_domains = [x.domain for x in g.db.query(BannedDomain.domain).all()]
+		banned_domains = [x.domain for x in g.db.query(BannedDomain.domain)]
 
 	for link in links:
 		#remove empty links

files/routes/admin.py
@@ -242,10 +242,10 @@ def revert_actions(v, username):
 
 	cutoff = int(time.time()) - 86400
 
-	posts = [x[0] for x in g.db.query(ModAction.target_post_id).filter(ModAction.user_id == revertee.id, ModAction.created_utc > cutoff, ModAction.kind == 'ban_post').all()]
+	posts = [x[0] for x in g.db.query(ModAction.target_post_id).filter(ModAction.user_id == revertee.id, ModAction.created_utc > cutoff, ModAction.kind == 'ban_post')]
 	posts = g.db.query(Post).filter(Post.id.in_(posts)).all()
 
-	comments = [x[0] for x in g.db.query(ModAction.target_comment_id).filter(ModAction.user_id == revertee.id, ModAction.created_utc > cutoff, ModAction.kind == 'ban_comment').all()]
+	comments = [x[0] for x in g.db.query(ModAction.target_comment_id).filter(ModAction.user_id == revertee.id, ModAction.created_utc > cutoff, ModAction.kind == 'ban_comment')]
 	comments = g.db.query(Comment).filter(Comment.id.in_(comments)).all()
 
 	for item in posts + comments:
@@ -254,7 +254,7 @@ def revert_actions(v, username):
 		item.is_approved = v.id
 		g.db.add(item)
 
-	users = (x[0] for x in g.db.query(ModAction.target_user_id).filter(ModAction.user_id == revertee.id, ModAction.created_utc > cutoff, ModAction.kind.in_(('shadowban', 'ban_user'))).all())
+	users = (x[0] for x in g.db.query(ModAction.target_user_id).filter(ModAction.user_id == revertee.id, ModAction.created_utc > cutoff, ModAction.kind.in_(('shadowban', 'ban_user'))))
 	users = g.db.query(User).filter(User.id.in_(users)).all()
 
 	for user in users:

commit 166f6fb4ee02803ed9260dda2a28380034f25ff9

fix 500 error caused by 3ec2038caf16d17dd35f3fcbd3b089bb765e8aab

files/classes/user.py
@@ -560,7 +560,7 @@ class User(Base):
 	@lazy
 	def all_twoway_blocks(self):
 		return set([x[0] for x in g.db.query(UserBlock.target_id).filter_by(user_id=self.id).all() + \
-			g.db.query(UserBlock.user_id).filter_by(target_id=self.id)])
+			g.db.query(UserBlock.user_id).filter_by(target_id=self.id).all()])
 
 
 	def validate_2fa(self, token):