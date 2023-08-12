1aac460298e3e282e8224d04be4348ef6c8df0da
Aevann <randomname42029@gmail.com>
2023-08-12 15:48:40 +0300

    restore accidentially removed lines

--- files/assets/js/core.js
+++ files/assets/js/core.js
@@ -33,6 +33,9 @@ function createXhrWithFormKey(url, form=new FormData()) {
 }
 
 function postToast(t, url, data, extraActionsOnSuccess, extraActionsOnFailure) {
+	t.disabled = true;
+	t.classList.add("disabled");
+
 	let form = new FormData();
 	if (typeof data === 'object' && data !== null) {
 		for(let k of Object.keys(data)) {
