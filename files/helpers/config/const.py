1db546e376921ac49cd5f45d9059ada94f2d98e2
Aevann <randomname42029@gmail.com>
2023-08-19 02:30:41 +0300

    do this https://rdrama.net/post/18459/marseycapywalking-megathread-for-bugs-and-suggestions/4827943#context

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -1110,6 +1110,7 @@ CHUD_PHRASES = (
 		"Climate action now",
 		"Long live the CCP",
 		"I stand with Ukraine",
+		"Leo Frank was innocent",
 	)
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
