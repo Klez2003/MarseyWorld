77b7ee26f29580e6dcb953b6d02518287ee81b3c
Aevann <randomname42029@gmail.com>
2023-09-06 00:38:58 +0300

    fix krayon filter

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -234,7 +234,7 @@ if SITE_NAME == 'rDrama':
 		"its almost like": "I'm a retard but",
 		"my brother in christ": "my brother in Allah (ﷻ)",
 		"(?<![a-z])cool (?!it|down|off)": "fetch ",
-		"krayon": "krayon (sister toucher)",
+		"krayon(?! \()": "krayon (sister toucher)",
 		"discord": "groomercord",
 		"allah(?! \()": "Allah (ﷻ)",
 		"my wife(?! \()": "my wife (male)",
