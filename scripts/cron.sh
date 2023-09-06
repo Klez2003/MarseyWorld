47b75483fa2b244cacf2856ff156b21e657893d9
Aevann <randomname42029@gmail.com>
2023-09-06 20:01:30 +0300

    add line for localhost

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -5,6 +5,7 @@ LOG='/var/log/rdrama/cron.log'
 echo -e "\n======================================== $1 === $(date --iso-8601=minutes)" >> "$LOG"
 
 source /e
+. .env
 /usr/local/bin/flask cron $1 >> "$LOG" 2>&1
 
 echo -e "======================================== $1 === $(date --iso-8601=minutes)" >> "$LOG"
