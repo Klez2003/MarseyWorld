4dc10e0f9930e55c4355e627396f38494e053556
SneedBot <rdrama@rdrama.net>
2023-09-07 12:09:56 +0000

    sneed

--- scripts/pitr.sh
+++ scripts/pitr.sh
@@ -1,17 +1,19 @@
-change recovery_target_time in /etc/postgresql/15/main/postgresql.conf like this
-recovery_target_time = '2023-08-12 06:07:00'
+change recovery_target_time in /var/lib/postgresql/15/new/postgresql.auto.conf like this
+recovery_target_time = '2023-09-07 3:18:24'
 
-systemctl stop postgresql@15-main
-mv /var/lib/postgresql/15/main/pg_wal /pg_wal
-rm -rf /var/lib/postgresql/15/main
-mkdir /var/lib/postgresql/15/main
-cp -a /database_backup/. /var/lib/postgresql/15/main
-chown postgres:postgres /var/lib/postgresql/15/main
-chmod 700 /var/lib/postgresql/15/main
-rm -rf /var/lib/postgresql/15/main/pg_wal
-mv /pg_wal /var/lib/postgresql/15/main/pg_wal
-touch /var/lib/postgresql/15/main/recovery.signal
-systemctl start postgresql@15-main
+systemctl stop postgresql@15-new
+
+rm -r /var/lib/postgresql/15/new
+cp -ar /database_backup/. /var/lib/postgresql/15/new
+chmod 700 /var/lib/postgresql/15/new
+
+rm -r /var/lib/postgresql/15/new/pg_wal
+cp -ar /var/lib/postgresql/15/main/pg_wal/. /var/lib/postgresql/15/new/pg_wal
+
+touch /var/lib/postgresql/15/new/recovery.signal
+systemctl start postgresql@15-new
+
+psql "${DATABASE_URL/5432/"5433"}"
 
 wait after recovery is done then:
-rm /var/lib/postgresql/15/main/recovery.signal
+rm /var/lib/postgresql/15/new/recovery.signal
