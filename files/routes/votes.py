a79a416cc2f498d00ce743df5ef9e2f8f407f94e
Aevann <randomname42029@gmail.com>
2023-09-08 23:40:32 +0300

    ultra-boost /h/countryclub and /h/highrollerclub

--- files/routes/votes.py
+++ files/routes/votes.py
@@ -136,11 +136,13 @@ def vote_post_comment(target_id, new, v, cls, vote_cls):
 	elif target.author.progressivestack or target.author.admin_level >= PERMS['IS_PERMA_PROGSTACKED']:
 		mul = 2
 	elif SITE == 'rdrama.net' and cls == Post:
-		if (target.domain.endswith('.win')
+		if target.sub in {'countryclub', 'highrollerclub'}:
+			mul = 3
+		elif (target.domain.endswith('.win')
 		or 'forum' in target.domain or 'chan' in target.domain or 'lemmy' in target.domain or 'mastodon' in target.domain
 		or (target.domain in BOOSTED_SITES and not target.url.startswith('/'))):
 			mul = 2
-		elif target.sub in STEALTH_HOLES or target.sub in {'countryclub', 'highrollerclub'}:
+		elif target.sub in STEALTH_HOLES:
 			mul = 2
 		elif 6 <= datetime.fromtimestamp(target.created_utc).hour <= 10:
 			mul = 2
