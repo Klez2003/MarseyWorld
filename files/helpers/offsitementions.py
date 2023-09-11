b7e3f7719e5657f3fa6df1639e752e237614b756
Aevann <randomname42029@gmail.com>
2023-09-11 21:06:41 +0300

    try smth to fix error

--- files/helpers/offsitementions.py
+++ files/helpers/offsitementions.py
@@ -41,7 +41,7 @@ def get_mentions(cache, queries, reddit_notifs_users=False):
 			url = f'https://api.pullpush.io/reddit/search/{kind}?q={query}'
 			data += requests.get(url, headers=HEADERS, timeout=5, proxies=proxies).json()['data']
 
-		data = sorted(data, key=lambda x: x['created_utc'], reverse=True)
+		data = sorted(data, key=lambda x: int(x['created_utc']), reverse=True)
 
 		for thing in data:
 			if thing['subreddit'] in {'IAmA', 'PokemonGoRaids', 'SubSimulatorGPT2', 'SubSimGPT2Interactive'}: continue
