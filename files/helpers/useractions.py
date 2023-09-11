be91cca7da8e1f8115e9ae40b37f0bf3969dc3db
Aevann <randomname42029@gmail.com>
2023-09-11 21:30:13 +0300

    delete unused parameter

--- files/helpers/useractions.py
+++ files/helpers/useractions.py
@@ -3,7 +3,7 @@ from flask import g
 from files.classes.badges import Badge
 from files.helpers.alerts import send_repeatable_notification
 
-def badge_grant(user, badge_id, description=None, url=None, notify=True, check_if_exists=True):
+def badge_grant(user, badge_id, description=None, url=None, notify=True):
 	g.db.flush()
 	existing = g.db.query(Badge).filter_by(user_id=user.id, badge_id=badge_id).one_or_none()
 	if existing: return
