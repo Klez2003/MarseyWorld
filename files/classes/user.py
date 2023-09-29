a14656de22432c94297cf83170c030f56df53f35
Aevann <randomname42029@gmail.com>
2023-09-29 09:09:59 +0300

    fix forced_hats

--- files/classes/user.py
+++ files/classes/user.py
@@ -343,7 +343,7 @@ class User(Base):
 		user_forced_hats = []
 		for k, val in forced_hats.items():
 			if getattr(self, k) and getattr(self, k) > 1:
-				if isinstance(val, tuple):
+				if isinstance(val[0], tuple):
 					user_forced_hats.append(random.choice(val))
 				else:
 					user_forced_hats.append(val)
