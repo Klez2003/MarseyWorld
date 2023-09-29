f97f1f160c3afae29b2e8d2aeceb85f8b5ff3811
Aevann <randomname42029@gmail.com>
2023-09-29 05:22:36 +0300

    Revert "make orgy youtube video autoplay (works only in these cases https://stackoverflow.com/a/50890610)"
    This reverts commit 2fe4c515655fa914c3a5ccb328922aad0ade1f42.

---
+++ files/assets/js/orgy_file.js
@@ -0,0 +1,40 @@
+const orgy_file = document.getElementById('orgy-file');
+const break_file = document.getElementById('break-file');
+
+addEventListener("load", () => {
+	orgy_file.play()
+});
+document.addEventListener('click', () => {
+	if (orgy_file.paused) orgy_file.play();
+}, {once : true});
+
+function add_playing_listener() {
+	orgy_file.addEventListener('playing', () => {
+		const now = Date.now() / 1000;
+		const created_utc = orgy_file.dataset.created_utc
+		orgy_file.currentTime = now - created_utc
+	}, {once : true});
+}
+
+add_playing_listener()
+
+orgy_file.addEventListener('pause', () => {
+	add_playing_listener()
+})
+
+orgy_file.addEventListener("timeupdate", function(){
+	if (break_file.dataset.run == "0" && parseInt(orgy_file.currentTime) == 3000) {
+		break_file.dataset.run = "1"
+		orgy_file.pause();
+		orgy_file.classList.add('d-none');
+		break_file.classList.remove('d-none');
+		break_file.play()
+		setTimeout(function () {
+			break_file.pause()
+			break_file.classList.add('d-none');
+			orgy_file.classList.remove('d-none');
+			orgy_file.dataset.created_utc = parseInt(orgy_file.dataset.created_utc) + 303
+			orgy_file.play()
+		}, 300000);
+	}
+});
