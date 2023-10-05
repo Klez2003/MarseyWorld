f03b88effbee1fc1b949387e452dadb66d1d4e6b
Aevann <randomname42029@gmail.com>
2023-10-05 13:46:03 +0300

    remove now-unused classmethod

--- files/classes/user.py
+++ files/classes/user.py
@@ -1154,41 +1154,6 @@ class User(Base):
 		tier_money = TIER_TO_MONEY[self.patron]
 		return f'{tier_name} - Donates ${tier_money}/month'
 
-	@classmethod
-	def can_see(cls, user, other):
-		if isinstance(other, (Post, Comment)):
-			if not cls.can_see(user, other.author): return False
-			if user and user.id == other.author_id: return True
-			if isinstance(other, Post):
-				if other.sub and not cls.can_see(user, other.subr):
-					return False
-				if request.headers.get("Cf-Ipcountry") == 'NZ':
-					if 'christchurch' in other.title.lower():
-						return False
-					if SITE == 'watchpeopledie.tv' and other.id in {5, 17212, 22653, 23814}:
-						return False
-			else:
-				if hasattr(other, 'is_blocking') and other.is_blocking and not request.path.endswith(f'/{other.id}'):
-					return False
-				if other.parent_post:
-					return cls.can_see(user, other.post)
-				else:
-					if not user and not other.wall_user_id: return False
-
-					if other.sentto:
-						if other.sentto == MODMAIL_ID:
-							if other.top_comment.author_id == user.id: return True
-							return user.admin_level >= PERMS['VIEW_MODMAIL']
-						if other.sentto != user.id:
-							return user.admin_level >= PERMS['BLACKJACK_NOTIFICATIONS']
-		elif isinstance(other, Sub):
-			if other.name == 'chudrama': return bool(user) and user.can_see_chudrama
-			if other.name == 'countryclub': return bool(user) and user.can_see_countryclub
-			if other.name == 'highrollerclub': return bool(user) and user.can_see_highrollerclub
-		elif isinstance(other, User):
-			return (user and user.id == other.id) or (user and user.can_see_shadowbanned) or not other.shadowbanned
-		return True
-
 	@property
 	@lazy
 	def can_see_restricted_holes(self):
