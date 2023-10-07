d7dda8eba5f112bac49427606f737d5c9be7b8d6
Aevann <randomname42029@gmail.com>
2023-10-07 21:14:00 +0300

    fix "too many packets in payload" error

--- files/assets/js/chat.js
+++ files/assets/js/chat.js
@@ -321,7 +321,7 @@ function send_hearbeat() {
 	socket.emit('heartbeat')
 }
 send_hearbeat()
-setInterval(send_hearbeat, 10000);
+setInterval(send_hearbeat, 20000);
 
 box.scrollTo(0, box.scrollHeight)
 setTimeout(function () {
