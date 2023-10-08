aae41912bba92df757ced615853008d76f87d17e
Aevann <randomname42029@gmail.com>
2023-10-08 23:56:27 +0300

    fix seed-emojis.sql

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -13,9 +13,9 @@ sed -i -E "s/INSERT INTO public.hat_defs VALUES \(.*, [0-9]{1,6}, [0-9]{10}\);//
 
 #generate seed-emojis.sql
 EXPORT_EMOJIS=$(psql --csv --tuples-only -P "null=NULL" -c \
-        "SELECT ''''||name||'''', ''''||kind||'''', 2, ''''||tags||'''' FROM emojis WHERE submitter_id IS NULL ORDER BY name" \
+        "SELECT ''''||name||'''', ''''||kind||'''', 2, ''''||tags||'''', ''''||nsfw||'''' FROM emojis WHERE submitter_id IS NULL ORDER BY name" \
         "$DATABASE_URL")
 EXPORT_EMOJIS=$(sed 's/.*/\(&\),/' <<< "$EXPORT_EMOJIS")
-echo "INSERT INTO public.emojis (name, kind, author_id, tags) VALUES" > "/d/seed-emojis.sql"
+echo "INSERT INTO public.emojis (name, kind, author_id, tags, nsfw) VALUES" > "/d/seed-emojis.sql"
 echo "${EXPORT_EMOJIS%?}" >> "/d/seed-emojis.sql"
 echo "ON CONFLICT (name) DO UPDATE SET tags = EXCLUDED.tags;" >> "/d/seed-emojis.sql"
