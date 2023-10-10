fd185eed041c4e241be17f68dd4fe533558144ce
Aevann <randomname42029@gmail.com>
2023-10-10 22:05:45 +0300

    use working teddit domain

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -134,7 +134,7 @@ def settings_personal_post(v):
 		updated = True
 	elif request.values.get("reddit", v.reddit) != v.reddit:
 		reddit = request.values.get("reddit")
-		if reddit in {'old.reddit.com', 'reddit.com', 'reddit.lol', 'libreddit.hu', 'undelete.pullpush.io'}:
+		if reddit in {'old.reddit.com', 'reddit.com', 'teddit.net', 'libreddit.hu', 'undelete.pullpush.io'}:
 			updated = True
 			v.reddit = reddit
 	elif request.values.get("poor", v.poor) != v.poor:
