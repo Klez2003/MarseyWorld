55f05bf95c922b79369e2cfddcf1c8727f87c47a
Aevann <randomname42029@gmail.com>
2023-10-12 22:43:35 +0300

    delete unused file

--- files/assets/events/DKD/js/music.js
+++
@@ -1,9 +0,0 @@
-const song = document.getElementById('event-song').value;
-const audio = new Audio(song);
-audio.loop = true;
-
-audio.play();
-document.addEventListener('click', () => {
-	if (audio.paused) audio.play();
-}, {once : true});
-prepare_to_pause(audio)
