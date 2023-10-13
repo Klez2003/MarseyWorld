6a80af912c57791e680d2cef4db15cf682a81d12
Chuck <dude@bussy.com>
2023-10-12 23:30:39 -0700

    Fix pushed after double down showing wrong visual indicator

--- files/assets/js/casino/blackjack_screen.js
+++ files/assets/js/casino/blackjack_screen.js
@@ -186,7 +186,7 @@ function updateCardsetBackgrounds(state, complete = false) {
 	if(complete){
 		const wager = state.has_player_split ? state?.wager?.amount * 2 : state?.wager?.amount;
 		let dealerShows = state.payout < wager ? 'WON': 'LOST';
-		if(state.payout === wager) dealerShows = 'PUSHED'
+		if(state.payout === wager || (state.player_doubled_down && state.status === 'PUSHED')) dealerShows = 'PUSHED'
 		cardsets[0]?.classList.add(`blackjack-cardset__${dealerShows}`)
 	}
 	else {
