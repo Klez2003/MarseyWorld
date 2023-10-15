64a1ef1cec64d02539b755347598351b29f4e3ae
Aevann <randomname42029@gmail.com>
2023-10-15 17:05:32 +0300

    remove pointless

--- files/assets/events/homoween/js/banner.js
+++ files/assets/events/homoween/js/banner.js
@@ -1,13 +1,9 @@
 const el = document.getElementById("banner-homoween-title");
 
-function animateBannerText() {
-	const letters = el.getElementsByTagName("tspan");
+const letters = el.getElementsByTagName("tspan");
 
-	for (let i = 0; i < letters.length; i++) {
-		letters.item(i).style.transition = `all 600ms ${600 + i * 40}ms`;
-	}
-
-	setTimeout(() => el.classList.add("life"), 1000);
+for (let i = 0; i < letters.length; i++) {
+	letters.item(i).style.transition = `all 600ms ${600 + i * 40}ms`;
 }
 
-animateBannerText();
+setTimeout(() => el.classList.add("life"), 1000);
