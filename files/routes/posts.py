5aca6c87b13b237b5f06b3c6dee12917bbef2ed2
Aevann <randomname42029@gmail.com>
2023-10-18 20:11:40 +0300

    Revert "remove LLM exception since hes permachudded now"
    This reverts commit 590664b3b0927dbe5ed32b210448c2cec6bb1114.

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -452,7 +452,7 @@ def submit_post(v, hole=None):
 
 	hole = request.values.get("hole", "").lower().replace('/h/','').strip()
 
-	if SITE == 'rdrama.net' and v.chud == 1:
+	if SITE == 'rdrama.net' and (v.chud == 1 or v.id == 253):
 		hole = 'chudrama'
 
 	if SITE == 'rdrama.net' and v.id == 10947:
