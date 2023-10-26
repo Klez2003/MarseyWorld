8b4682268eb5e12936cef10316482834c9b39e6e
Aevann <randomname42029@gmail.com>
2023-10-27 02:16:49 +0300

    fix sharpen + chud with "trans women are women" phrase

--- files/helpers/sharpen.py
+++ files/helpers/sharpen.py
@@ -1,14 +1,15 @@
 from files.helpers.regex import *
+import re
 
-def sharpen(string, v):
-	if v.chud_phrase:
-		string = string.replace(v.chud_phrase, 'erfdsx34224e4535resfed')
+def sharpen(string, chud_phrase):
+	if chud_phrase:
+		string = re.sub(chud_phrase, 'erfdsx34224e4535resfed', string, flags=re.I)
 
 	string = the_fucking_regex.sub("\g<1> fucking", string)
 	string = bitch_question_mark_regex.sub(", bitch?", string)
 	string = exclamation_point_regex.sub(", motherfucker!", string)
 
-	if v.chud_phrase:
-		string = string.replace('erfdsx34224e4535resfed', v.chud_phrase)
+	if chud_phrase:
+		string = string.replace('erfdsx34224e4535resfed', chud_phrase)
 
 	return string
