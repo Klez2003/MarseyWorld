316610f8e45227abed2a3a606d1eac66d31133b8
SneedBot <rdrama@rdrama.net>
2023-10-26 14:02:12 +0000

    sneed

--- seed-emojis-WPD.sql
+++ seed-emojis-WPD.sql
@@ -1,3 +1,4 @@
 INSERT INTO public.emojis (name, kind, author_id, tags, nsfw, created_utc) VALUES
-()
+('marseyaward2','Marsey',2,'chair obama',false,1698326564),
+('marseywhale','Marsey',2,'marsey fat diabetic obese mcdonalds whale fries burger soda',false,1698326462)
 ON CONFLICT (name) DO UPDATE SET tags = EXCLUDED.tags;
