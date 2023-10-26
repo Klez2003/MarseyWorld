0d88be92610fb39d02dc09b0ceb466ed8f225bad
SneedBot <rdrama@rdrama.net>
2023-10-26 18:02:14 +0000

    sneed

--- seed-emojis-WPD.sql
+++ seed-emojis-WPD.sql
@@ -5,6 +5,7 @@ INSERT INTO public.emojis (name, kind, author_id, tags, nsfw, created_utc) VALUE
 ('marseybeingnerd','Marsey',2,'dork dweeb glasses',false,1698330357),
 ('marseyconstipation','Marsey',2,'shit poop diarrhea litter  constipated',false,1698329319),
 ('marseyeyeball','Marsey',2,'gore bloody',false,1698332084),
+('marseynerdy','Marsey',2,'nerd marsey loser reading book',false,1698342535),
 ('marseywhale','Marsey',2,'marsey fat diabetic obese mcdonalds whale fries burger soda',false,1698326462),
 ('minioncocksock','Misc',2,'cocksleeve',true,1698334044),
 ('wojakselfsuck','Wojak',2,'penis cock',true,1698333394)
