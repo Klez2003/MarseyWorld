b220c38587f615ade4deaf9b45f54a4f65e9e8e0
SneedBot <rdrama@rdrama.net>
2023-10-28 19:02:12 +0000

    sneed

--- seed-emojis-WPD.sql
+++ seed-emojis-WPD.sql
@@ -9,8 +9,11 @@ INSERT INTO public.emojis (name, kind, author_id, tags, nsfw, created_utc) VALUE
 ('marseyinpixels', 'Marsey', 2, 'sprite 8bit', false, 1698343460),
 ('marseyjoyus', 'Marsey', 2, 'happy grin bucktooth yippie', false, 1698344674),
 ('marseynerdy', 'Marsey', 2, 'nerd marsey loser reading book', false, 1698342535),
+('marseypinkk', 'Marsey', 2, 'pink strawberry scarf cold ill sick', false, 1698496927),
 ('marseytortured', 'Marsey', 2, 'slitwrists crying sad tears blood cuts tortured mutilated gore', false, 1698367179),
+('marseyusatan', 'Marsey', 2, 'usagi', false, 1698470278),
 ('marseywhale', 'Marsey', 2, 'marsey fat diabetic obese mcdonalds whale fries burger soda', false, 1698326462),
+('marseywhat', 'Marsey', 2, 'disgust huh', false, 1698463302),
 ('minioncocksock', 'Misc', 2, 'cocksleeve', true, 1698334044),
 ('wojakselfsuck', 'Wojak', 2, 'penis cock', true, 1698333394),
 ('wolfamongus', 'Wolf', 2, 'sussy', false, 1698413110)
