20b09b890b8bc6b6f8058c5a8775f9f49a30d840
Aevann <randomname42029@gmail.com>
2023-11-01 01:46:54 +0300

    increase wpd threshold to 3500

--- files/routes/wrappers.py
+++ files/routes/wrappers.py
@@ -57,7 +57,7 @@ def calc_users():
 		g.loggedout_counter = len(loggedout)
 
 		if SITE == 'watchpeopledie.tv':
-			ddos_threshold = 3000
+			ddos_threshold = 3500
 		else:
 			ddos_threshold = 1000
 
