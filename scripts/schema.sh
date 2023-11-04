108a4e2929b9e50ffb8dbbd82d2eaa3197df0345
Aevann <randomname42029@gmail.com>
2023-11-04 18:53:42 +0200

    order seed-badges.sql by badge id

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -5,7 +5,13 @@
 pg_dump -O -x --schema-only "$DATABASE_URL" > '/d/schema.sql'
 
 #generate seed-badges.sql
-pg_dump -O -x --data-only --inserts -t 'badge_defs' "$DATABASE_URL" > "/d/seed-badges.sql"
+EXPORT_BADGES=$(psql --csv --tuples-only -P "null=NULL" -c \
+        "SELECT ''||id||'', ' '''||name||'''', ' '''||description||'''', ' '||created_utc||'' FROM badge_defs ORDER BY id" \
+        "$DATABASE_URL")
+EXPORT_BADGES=$(sed 's/.*/\(&\),/' <<< "$EXPORT_BADGES")
+echo "INSERT INTO public.badge_defs VALUES" > "/d/seed-badges.sql"
+echo "${EXPORT_BADGES%?}" >> "/d/seed-badges.sql"
+echo "ON CONFLICT (id) DO nothing;" >> "/d/seed-badges.sql"
 
 #generate seed-hats.sql
 pg_dump -O -x --data-only --inserts -t 'hat_defs' "$DATABASE_URL" > "/d/seed-hats.sql"
