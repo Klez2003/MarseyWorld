00ece38e42530bfc7f45a99526c329296fb656cb
Aevann <randomname42029@gmail.com>
2023-12-01 21:38:16 +0200

    add sql command

--- migrations/20231201-better-grinch-code.sql
+++ migrations/20231201-better-grinch-code.sql
@@ -1,3 +1,4 @@
 alter table users add column grinch bool default false not null;
 alter table users alter column grinch drop default;
 update users set grinch=true where id in (select user_id from badges where badge_id in (91,185));
+update award_relationships set kind='grinch' where kind='hallowgrinch';
