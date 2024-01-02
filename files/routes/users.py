a4c21d118c2646f140bb25cc4fe9a914c13cf546
Aevann <randomname42029@gmail.com>
2024-01-02 20:09:57 +0200

    try smth

--- files/routes/users.py
+++ files/routes/users.py
@@ -1365,6 +1365,9 @@ def gumroad():
 @limiter.exempt
 def bm():
 	data = json.loads(request.data)
+
+	print(data, flush=True)
+
 	ip = request.headers.get('CF-Connecting-IP')
 
 	if ip not in {'54.187.174.169','54.187.205.235','54.187.216.72'}:
