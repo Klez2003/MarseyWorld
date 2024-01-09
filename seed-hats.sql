1456fcd4b883a3b43ba331fb7b88fe32fe541833
SneedBot <rdrama@rdrama.net>
2024-01-09 04:00:15 +0000

    sneed

--- seed-hats.sql
+++ seed-hats.sql
@@ -1152,11 +1152,12 @@ INSERT INTO public.hat_defs VALUES (1383, 'Patel Petals', 'Pretty pastel petals 
 
 
 
+
 --
 -- Name: hat_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hat_defs_id_seq', 1392, true);
+SELECT pg_catalog.setval('public.hat_defs_id_seq', 1393, true);
 
 
 --
