142be891f6838e019d8367c113813f6868a2161a
Aevann <randomname42029@gmail.com>
2024-01-14 14:25:24 +0200

    remove linebreaks

--- files/routes/holes.py
+++ files/routes/holes.py
@@ -392,14 +392,10 @@ def create_sub2(v):
 	text = f":!marseyparty: /h/{hole} has been created by @{v.username} :marseyparty:"
 	text_html = sanitize(text, blackjack="notification")
 	cid = create_comment(text_html)
-
 	t = time.time() - 604800
 	notified_users = [x[0] for x in g.db.query(User.id).filter(User.admin_level >= PERMS['NOTIFICATIONS_HOLE_CREATION'], User.id != v.id, User.last_active > t)]
-
 	for uid in notified_users:
 		add_notif(cid, uid, text, check_existing=False)
-
-
 
 	return redirect(f"/h/{hole}")
 
