1ac79577ae8ec85b2789d345b1704b6a87c163ec
Aevann <randomname42029@gmail.com>
2024-01-31 21:20:21 +0200

    id_after -> permalink_after

--- files/assets/js/post_navigation.js
+++ files/assets/js/post_navigation.js
@@ -1,12 +1,12 @@
 const post_permalinks = sessionStorage.getItem("post_permalinks").split(', ');
 const current_index = post_permalinks.indexOf(`'${location.href}'`)
 if (current_index > -1) {
-	const id_after = post_permalinks[current_index+1]
+	const permalink_after = post_permalinks[current_index+1]
 
-	if (id_after) {
+	if (permalink_after) {
 		for (const btn of document.getElementsByClassName('post_navigation')) {
 			btn.classList.remove('d-none')
-			btn.href = id_after.slice(1, -1)
+			btn.href = permalink_after.slice(1, -1)
 		}
 	}
 }
