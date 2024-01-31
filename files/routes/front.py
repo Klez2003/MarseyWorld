d2b8379ecc9f8cad97712a5ff44f341dc030e866
Aevann <randomname42029@gmail.com>
2024-01-31 21:56:53 +0200

    dont allow posts from /h/pets to appear in the first page of the frontpage

--- files/routes/front.py
+++ files/routes/front.py
@@ -147,6 +147,9 @@ def frontlist(v=None, sort="hot", page=1, t="all", ids_only=True, filter_words='
 	posts = posts.options(load_only(Post.id)).offset(size * (page - 1))
 
 	if SITE_NAME == 'WPD' and sort == "hot" and hole == None:
+		if page == 1:
+			posts = posts.filter(Post.hole != 'pets')
+	
 		posts = posts.limit(200).all()
 		posts_in_limited_holes = [x for x in posts if x.hole in LIMITED_WPD_HOLES]
 		captured_holes = set()
