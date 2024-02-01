65d85c5991b30eb47ba8b3252a40b5d38bade016
Aevann <randomname42029@gmail.com>
2024-02-01 21:46:25 +0200

    geoblock

--- files/routes/static.py
+++ files/routes/static.py
@@ -432,3 +432,13 @@ def donate(v):
 @auth_required
 def orgy(v):
 	return redirect("/chat")
+
+if SITE == 'watchpeopledie.tv':
+	@app.get("/1706708037797343.mp4")
+	@limiter.limit(DEFAULT_RATELIMIT, deduct_when=lambda response: response.status_code < 400)
+	@limiter.limit(DEFAULT_RATELIMIT, deduct_when=lambda response: response.status_code < 400, key_func=get_ID)
+	@auth_required
+	def geoblock(v):
+		if request.headers.get("Cf-Ipcountry") == 'AU':
+			abort(403)
+		return send_file('/videos/1706708037797343.mp4')
