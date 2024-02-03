f2b8e1c6c5ebfbeb991c6f1abe7783ba72fde1a3
Aevann <randomname42029@gmail.com>
2024-02-03 05:42:04 +0200

    im retardo

--- files/assets/js/post_navigation.js
+++ files/assets/js/post_navigation.js
@@ -1,12 +1,16 @@
-const post_permalinks = sessionStorage.getItem("post_permalinks").split(', ');
-const current_index = post_permalinks.indexOf(`'${location.href}'`)
-if (current_index > -1) {
-	const permalink_after = post_permalinks[current_index+1]
+let post_permalinks = sessionStorage.getItem("post_permalinks")
 
-	if (permalink_after) {
-		for (const btn of document.getElementsByClassName('post_navigation')) {
-			btn.classList.remove('d-none')
-			btn.href = permalink_after.slice(1, -1)
+if (post_permalinks) {
+	post_permalinks = post_permalinks.split(', ');
+	const current_index = post_permalinks.indexOf(`'${location.href}'`)
+	if (current_index > -1) {
+		const permalink_after = post_permalinks[current_index+1]
+
+		if (permalink_after) {
+			for (const btn of document.getElementsByClassName('post_navigation')) {
+				btn.classList.remove('d-none')
+				btn.href = permalink_after.slice(1, -1)
+			}
 		}
 	}
 }
