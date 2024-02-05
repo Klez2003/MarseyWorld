d76d1dfbbec1fabfce981594665a4a59064e1664
Aevann <randomname42029@gmail.com>
2024-02-06 01:52:34 +0200

    do what dwhite wants

--- files/helpers/alerts.py
+++ files/helpers/alerts.py
@@ -162,7 +162,7 @@ def NOTIFY_USERS(text, v, oldtext=None, ghost=False, obj=None, followers_ping=Tr
 	user_ids = get_users(names, ids_only=True, graceful=True)
 	notify_users.update(user_ids)
 
-	if SITE_NAME == "WPD" and 'daisy' in text:
+	if SITE_NAME == "WPD" and ('daisy' in text or 'kill myself' in text):
 		admin_ids = [x[0] for x in g.db.query(User.id).filter(User.admin_level >= PERMS['NOTIFICATIONS_SPECIFIC_WPD_COMMENTS'])]
 		notify_users.update(admin_ids)
 
