43d768f0aa61d7e75f10ce81561ac86265bbb369
Aevann <randomname42029@gmail.com>
2024-02-12 10:44:05 +0200

    fix showmore

--- files/helpers/bleach_body.py
+++ files/helpers/bleach_body.py
@@ -7,7 +7,7 @@ from files.helpers.regex import sanitize_url_regex
 from files.helpers.config.const import *
 
 allowed_tags = ('a','audio','b','big','blink','blockquote','br','center','code','del','details','em','g','gl','h1','h2','h3','h4','h5','h6','hr','i','img','li','lite-youtube','marquee','ol','p','pre','rp','rt','ruby','small','span','spoiler','strike','strong','sub','summary','sup','table','tbody','td','th','thead','tr','u','ul','video')
-allowed_tags_runstime = ('div', 'input', 'label', 'score', 'button')
+allowed_tags_runstime = ('div', 'input', 'label', 'score', 'button', 'd')
 allowed_css_properties = ('background-color', 'color', 'filter', 'font-weight', 'text-align', 'transform')
 
 def allowed_attributes(tag, name, value):
