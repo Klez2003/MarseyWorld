667fec0dec186ad6ac0cc971a89fafcb3ab50610
Aevann <randomname42029@gmail.com>
2024-02-13 22:28:28 +0200

    add missing db.close() and rollback

--- files/routes/jinja2.py
+++ files/routes/jinja2.py
@@ -120,6 +120,8 @@ def bar_position():
 		"FULL OUTER JOIN (SELECT id AS uid FROM users WHERE zombie != 0) as q2 "
 		"ON q1.uid = q2.uid")).one()[0]
 	total = max(total, 1)
+	db.rollback()
+	db.close()
 
 	return [int((vaxxed * 100) / total), int((zombie * 100) / total), vaxxed, zombie]
 
