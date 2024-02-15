ecd64e1999a02d4b39c6e583e902346a8f8ec39e
TriHard <dude@bussy.lmao>
2024-02-15 06:17:23 -0700

    declarative_base -> DeclarativeBase

--- files/classes/__init__.py
+++ files/classes/__init__.py
@@ -1,6 +1,7 @@
 # load sqlalchemy's declarative base...
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from sqlalchemy.orm import DeclarativeBase
+class Base(DeclarativeBase):
+	pass
 
 # then load all of our classes :)
 from .alts import *
