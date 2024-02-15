7785dae3a8ffc4ba2e3c99c4dd241193ddd13bd3
Aevann <randomname42029@gmail.com>
2024-02-16 00:05:33 +0200

    remove now-unnecessary bool

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -63,7 +63,7 @@ def publish(pid, v):
 	g.db.add(p)
 
 	p.chudded = v.chud and p.hole != 'chudrama' and not (p.is_longpost and not v.chudded_by)
-	p.queened = bool(v.queen and not p.is_longpost)
+	p.queened = v.queen and not p.is_longpost
 	p.sharpened = v.sharpen and not p.is_longpost
 	p.rainbowed = v.rainbow and not p.is_longpost
 
