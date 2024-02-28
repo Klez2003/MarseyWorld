90720233ebad869f7cd39a8b364fe3259e10a420
Aevann <randomname42029@gmail.com>
2024-02-28 20:01:54 +0200

    less downtime

--- migrations/20240228-fix-exact-searching.sql
+++ migrations/20240228-fix-exact-searching.sql
@@ -1,2 +1,3 @@
+alter table comments add column body_ts2 tsvector GENERATED ALWAYS AS (to_tsvector('simple'::regconfig, (body)::text)) STORED;
 alter table comments drop column body_ts;
-alter table comments add column body_ts tsvector GENERATED ALWAYS AS (to_tsvector('simple'::regconfig, (body)::text)) STORED;
+alter table comments rename column body_ts2 to body_ts;
