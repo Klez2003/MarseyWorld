9ebdf46c62c5919a81066bf16cb5e97e351fbdb4
Aevann <randomname42029@gmail.com>
2024-02-28 17:33:01 +0200

    fix this https://rdrama.net/post/249465/new-toss-marseysus/6023877#context

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -481,6 +481,9 @@ def submit_post(v, hole=None):
 	if SITE == 'rdrama.net' and (v.chud == 1 or v.id == 253):
 		hole = 'chudrama'
 
+	if SITE == 'rdrama.net' and v.id == 4358:
+		hole = 'slavshit'
+
 	if SITE == 'rdrama.net' and v.id == 10947:
 		hole = 'mnn'
 
