285007ae78d5a76a2d9f58511a2cfcf8c59ecda2
Aevann <randomname42029@gmail.com>
2024-03-02 13:39:24 +0200

    no notifs on wpd

--- files/classes/user.py
+++ files/classes/user.py
@@ -820,7 +820,10 @@ class User(Base):
 	def modmail_notifications_count(self):
 		if self.admin_level < PERMS['NOTIFICATIONS_MODMAIL']:
 			return 0
-		
+
+		if self.id == AEVANN_ID and SITE_NAME == 'WPD':
+			return 0
+
 		return g.db.query(Comment).distinct(Comment.top_comment_id).filter(
 				Comment.author_id != self.id,
 				Comment.sentto == MODMAIL_ID,
@@ -855,7 +858,7 @@ class User(Base):
 	@property
 	@lazy
 	def modaction_notifications_count(self):
-		if self.id == AEVANN_ID and SITE_NAME != 'rDrama':
+		if self.id == AEVANN_ID and SITE_NAME == 'WPD':
 			return 0
 
 		if self.admin_level >= PERMS['NOTIFICATIONS_MODERATOR_ACTIONS']:
