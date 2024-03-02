02f1ba52d0b2bd744ab1f20f83ba27572314dbe1
Aevann <randomname42029@gmail.com>
2024-03-02 20:23:06 +0200

    fix 500 error

--- files/helpers/alerts.py
+++ files/helpers/alerts.py
@@ -235,7 +235,7 @@ def NOTIFY_USERS(text, v, oldtext=None, ghost=False, obj=None, followers_ping=Tr
 				if group and group.name == 'verifiedrich':
 					abort(403, f"Only !verifiedrich members can mention it!")
 				cost += len(members) * 5
-				cost_groups.append(group.name)
+				cost_groups.append(i.group(1))
 				if cost > v.coins + v.marseybux:
 					abort(403, f"You need {cost} currency to mention these ping groups!")
 
