8ebc07973f00548d054793e9a7b12a7c835f0b73
Aevann <randomname42029@gmail.com>
2024-03-05 23:37:00 +0200

    fix inaccurate number

--- files/assets/js/deaths_today.js
+++ files/assets/js/deaths_today.js
@@ -1,11 +1,11 @@
 const dt = new Date();
-let death_secs = dt.getSeconds() + (60 * dt.getMinutes()) + (60 * 60 * dt.getHours());
+let death_counter = parseInt(dt.getSeconds() + (60 * dt.getMinutes()) + (60 * 60 * dt.getHours()) * 1.951);
 
 function update_death_counter() {
-	death_secs += 1
+	death_counter += 1
 	for (const el of document.getElementsByClassName('deaths-today'))
-		el.innerHTML = death_secs.toLocaleString()
+		el.innerHTML = death_counter.toLocaleString()
 }
 
 update_death_counter()
-setInterval(update_death_counter, 518);
+setInterval(update_death_counter, 512);
