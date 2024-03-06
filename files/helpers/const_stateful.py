35260d840ee571c2badb3fd414ae5867f606874b
Aevann <randomname42029@gmail.com>
2024-03-07 00:25:45 +0200

    actually fix error

--- files/helpers/const_stateful.py
+++ files/helpers/const_stateful.py
@@ -35,7 +35,9 @@ def const_initialize():
 	SNAPPY_KONGS = db.query(Emoji.name).filter(Emoji.kind=="Donkey Kong", Emoji.submitter_id==None, Emoji.nsfw == False).all()
 	SNAPPY_KONGS = [f':#{x[0]}:' for x in SNAPPY_KONGS]
 
-	GIGABOOSTED_HOLES = [x[0] for x in db.query(Hole.name).filter_by(stealth=True)] + ['highrollerclub','countryclub'] - 'chudrama'
+	GIGABOOSTED_HOLES = [x[0] for x in db.query(Hole.name).filter_by(stealth=True)] + ['highrollerclub','countryclub']
+	if 'chudrama' in GIGABOOSTED_HOLES:
+		GIGABOOSTED_HOLES.remove('chudrama')
 
 	NSFW_EMOJIS = [x[0] for x in db.query(Emoji.name).filter_by(nsfw=True)]
 
