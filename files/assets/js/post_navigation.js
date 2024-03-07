d4ead1150f7e3e20412fe6cf3fa42d2b29438445
Aevann <randomname42029@gmail.com>
2024-03-07 23:14:39 +0200

    make "next post" button skip visited posts

--- files/assets/js/post_navigation.js
+++ files/assets/js/post_navigation.js
@@ -2,11 +2,18 @@ let post_permalinks = sessionStorage.getItem("post_permalinks")
 
 if (post_permalinks) {
 	post_permalinks = post_permalinks.split(', ');
-	const current_index = post_permalinks.indexOf(`'${location.href}'`)
+	let current_index = post_permalinks.indexOf(`'${location.href}'`)
 	if (current_index > -1) {
-		const permalink_after = post_permalinks[current_index+1]
+		let permalink_after = post_permalinks[current_index+1]
 
 		if (permalink_after) {
+			let pid = permalink_after.split('/').slice(-2, -1)[0]
+			while (comments[pid]) {
+				current_index += 1
+				permalink_after = post_permalinks[current_index]
+				pid = permalink_after.split('/').slice(-2, -1)[0]
+			}
+
 			for (const btn of document.getElementsByClassName('post_navigation')) {
 				btn.classList.remove('d-none')
 				btn.href = permalink_after.slice(1, -1)
