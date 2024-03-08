0529a3d1bd39727df0a82446e9b2f55bffb72635
Aevann <randomname42029@gmail.com>
2024-03-08 07:35:37 +0200

    fix images

--- files/assets/js/messages.js
+++ files/assets/js/messages.js
@@ -3,7 +3,7 @@ const socket = io()
 socket.on('insert_reply', function(data) {
 	const replies = document.getElementById(`replies-of-c_${data[0]}`)
 	if (replies) {
-		const html = data[1].replace(/data-nonce=".*?"/g, `data-nonce="${nonce}"`);
+		const html = data[1].replace(/data-src/g, 'src').replace(/data-nonce=".*?"/g, `data-nonce="${nonce}"`);
 
 		replies.insertAdjacentHTML('beforeend', html);
 
