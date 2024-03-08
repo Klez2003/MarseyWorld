10864430b2d6f028dc5885e3ff2c51f33632c8fd
Aevann <randomname42029@gmail.com>
2024-03-08 06:12:40 +0200

    fix this https://rdrama.net/notification/6072322#context

--- files/routes/chat.py
+++ files/routes/chat.py
@@ -332,10 +332,11 @@ def messagereply(v):
 
 	if user_id and user_id not in {v.id, MODMAIL_ID} | BOT_IDs:
 		if can_see(user, v):
-			notif = g.db.query(Notification).filter_by(comment_id=c.id, user_id=user_id).one_or_none()
-			if not notif:
-				notif = Notification(comment_id=c.id, user_id=user_id)
-				g.db.add(notif)
+			for user_id in c.group_dm_ids[1:]:
+				notif = g.db.query(Notification).filter_by(comment_id=c.id, user_id=user_id).one_or_none()
+				if not notif:
+					notif = Notification(comment_id=c.id, user_id=user_id)
+					g.db.add(notif)
 
 		title = f'New message from @{c.author_name}'
 		url = f'{SITE_FULL}/notifications/messages'
