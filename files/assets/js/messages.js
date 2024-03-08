a29655e0bace40df2c07ac4ca552cc4961cf8a44
Aevann <randomname42029@gmail.com>
2024-03-08 07:30:01 +0200

    fix `just now` timestamp

--- files/assets/js/messages.js
+++ files/assets/js/messages.js
@@ -3,7 +3,9 @@ const socket = io()
 socket.on('insert_reply', function(data) {
 	const replies = document.getElementById(`replies-of-c_${data[0]}`)
 	if (replies) {
-		replies.insertAdjacentHTML('beforeend', data[1]);
+		const html = data[1].replace(/data-nonce=".*?"/g, `data-nonce="${nonce}"`);
+
+		replies.insertAdjacentHTML('beforeend', html);
 
 		register_new_elements(replies.lastElementChild);
 		bs_trigger(replies.lastElementChild);
