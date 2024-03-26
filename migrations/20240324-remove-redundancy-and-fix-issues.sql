fe800762a18f9c2af3e842315488d730e5e338f6
Aevann <randomname42029@gmail.com>
2024-03-27 01:20:00 +0200

    same as last commit

--- migrations/20240324-remove-redundancy-and-fix-issues.sql
+++ migrations/20240324-remove-redundancy-and-fix-issues.sql
@@ -1,9 +1,4 @@
 alter table chat_memberships add column notification bool not null default false;
 alter table chat_memberships alter column notification drop default;
 
-alter table chat_memberships add column last_notified int;
-update chat_memberships a set last_notified=(select created_utc from chat_messages b where a.chat_id=b.chat_id order by created_utc desc limit 1);
-update chat_memberships set last_notified=0 where last_notified is null;
-alter table chat_memberships alter column last_notified set not null;
-
 drop table chat_notifications;
