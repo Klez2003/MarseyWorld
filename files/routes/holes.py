e879b8d34a9abc94231c73acd5d41d77d1b22b93
Aevann <randomname42029@gmail.com>
2024-04-05 10:45:26 +0200

    uncuck /h/braincels and remove a dead hole

--- files/routes/holes.py
+++ files/routes/holes.py
@@ -785,7 +785,7 @@ def hole_unpin(v, pid):
 @auth_required
 def hole_stealth(v, hole):
 	hole = get_hole(hole)
-	if hole.name in {'braincels','smuggies','mnn','glory','racist'} and v.admin_level < PERMS["MODS_EVERY_HOLE"]:
+	if hole.name in {'mnn','glory','racist'} and v.admin_level < PERMS["MODS_EVERY_HOLE"]:
 		abort(403)
 	if not v.mods_hole(hole.name): abort(403)
 
