889624f81d422c7864894fc914ee562e2b23bfd2
Aevann <randomname42029@gmail.com>
2024-04-08 15:02:24 +0200

    fix reddit embed height on mobile

--- files/assets/js/reddit_embed.js
+++ files/assets/js/reddit_embed.js
@@ -1,7 +1,10 @@
 addEventListener("message", function(t) {
 	if (typeof t.data == "string" && t.data) {
 		const data = JSON.parse(t.data)
+		let height = data.data
 		if (data && "type" in data && data.type == "resize.embed")
-			document.getElementById('reddit-embed').height = data.data
+			if (screen_width < 768)
+				height += 75
+			document.getElementById('reddit-embed').height = height
 	}
 })
