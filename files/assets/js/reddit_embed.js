4f65791d3bbe4fa18477581c3dd37e8f09dff989
Aevann <randomname42029@gmail.com>
2024-04-09 03:51:24 +0200

    cosmetic improvement to reddit embed

--- files/assets/js/reddit_embed.js
+++ files/assets/js/reddit_embed.js
@@ -3,8 +3,6 @@ addEventListener("message", function(t) {
 		const data = JSON.parse(t.data)
 		let height = data.data
 		if (data && "type" in data && data.type == "resize.embed")
-			if (screen_width < 768)
-				height += 40
 			document.getElementById('reddit-embed').height = height
 	}
 })
