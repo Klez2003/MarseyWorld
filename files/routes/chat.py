a51125e21b475b9a2f97c9f2b01a14db23ed84c0
Aevann <randomname42029@gmail.com>
2024-04-09 17:02:56 +0200

    dont send push notif when shadowbeaned

--- files/routes/chat.py
+++ files/routes/chat.py
@@ -379,12 +379,11 @@ def messagereply(v):
 	execute_blackjack(v, c, c.body_html, 'message')
 	execute_under_siege(v, c, c.body_html, 'message')
 
-	if user_id and user_id not in {v.id, MODMAIL_ID} | BOT_IDs and user_id not in online["messages"]:
-		if can_see(user, v):
-			notif = g.db.query(Notification).filter_by(comment_id=c.id, user_id=user_id).one_or_none()
-			if not notif:
-				notif = Notification(comment_id=c.id, user_id=user_id)
-				g.db.add(notif)
+	if user_id and user_id not in {v.id, MODMAIL_ID} | BOT_IDs and user_id not in online["messages"] and can_see(user, v):
+		notif = g.db.query(Notification).filter_by(comment_id=c.id, user_id=user_id).one_or_none()
+		if not notif:
+			notif = Notification(comment_id=c.id, user_id=user_id)
+			g.db.add(notif)
 
 		title = f'New message from @{c.author_name}'
 		url = f'{SITE_FULL}/notifications/messages'
