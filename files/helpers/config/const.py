0f0de6e63e3fbeb771ed20f365265265ef28c274
Aevann <randomname42029@gmail.com>
2024-04-10 12:22:49 +0200

    add longpostbot reply

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -388,6 +388,7 @@ LONGPOSTBOT_REPLIES = (
 	"Why?",
 	"https://i.rdrama.net/images/17051203593367493.webp",
 	"https://i.rdrama.net/images/17053964397685544.webp",
+	"https://i.rdrama.net/images/165178832073224.webp",
 )
 
 CHUD_MSGS = (
