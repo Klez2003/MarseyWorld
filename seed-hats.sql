5af6c3e76fe2e108ee07696850644ad402b541ec
SneedBot <rdrama@rdrama.net>
2024-04-13 00:00:16 +0000

    sneed

--- seed-hats.sql
+++ seed-hats.sql
@@ -1161,7 +1161,8 @@ INSERT INTO public.hat_defs VALUES (1513, 'pkmn0272', 'Ludicolo, the Carefree Po
 INSERT INTO public.hat_defs VALUES (1517, 'Malta Flag', 'Repubblika ta'' Malta', 2, 1000, NULL, 1711671657);
 INSERT INTO public.hat_defs VALUES (1525, 'Blood', 'Splash!', 2, 1000, NULL, 1711678746);
 INSERT INTO public.hat_defs VALUES (1528, 'Swirl Splash Effect', 'Swploosh', 2, 1000, NULL, 1711679915);
-
+INSERT INTO public.hat_defs VALUES (1535, 'Miku Frame', 'Show your love for the only thing that will out live roaches in the apocalypse, Hatsune Miku !!', 2, 500, NULL, 1712893209);
+INSERT INTO public.hat_defs VALUES (1536, 'Sleeping Konata Hat', 'All the way from the anime series Lucky Star to the top of your head!', 2, 500, NULL, 1712895608);
 INSERT INTO public.hat_defs VALUES (1450, 'Red Eminent Horns', 'Queen of the devils, leader of the chuds.', 2, 500, NULL, 1708367282);
 INSERT INTO public.hat_defs VALUES (1377, 'pkmn0094', 'Gengar, the Shadow Pokémon. A Ghost and Poison type. Gengar can pass through dimensions and appear almost anywhere. Since Gengar absorbs any warmth around it, the temperature suddenly drops wherever it appears.', 2, 500, NULL, 1698658745);
 INSERT INTO public.hat_defs VALUES (1376, 'pkmn0039', 'Jigglypuff, a Balloon Pokémon. It has large friendly eyes and it sings a pleasant song.', 2, 500, NULL, 1698658603);
@@ -1256,7 +1257,6 @@ INSERT INTO public.hat_defs VALUES (1530, 'Purple Aura', 'This person has a purp
 INSERT INTO public.hat_defs VALUES (1532, 'Polaroid photo', 'Ahhh I remember when I was young. How the time flies...', 2, 500, NULL, 1712164155);
 INSERT INTO public.hat_defs VALUES (1533, 'Emu Otori Hat', 'Show your love to the amazing idol Emu Otori!', 2, 500, NULL, 1712599460);
 INSERT INTO public.hat_defs VALUES (1534, 'Kitty Pawz', 'Kawaii? Sure. But deep down, we all know you’re a furry.', 2, 500, NULL, 1712799964);
-
 
 
 --
