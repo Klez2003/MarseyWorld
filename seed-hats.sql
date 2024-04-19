42862deb8e46f348a14a5c50f290bd67e348022d
SneedBot <rdrama@rdrama.net>
2024-04-19 16:00:17 +0000

    sneed

--- seed-hats.sql
+++ seed-hats.sql
@@ -1189,6 +1189,7 @@ INSERT INTO public.hat_defs VALUES (1529, 'Tornado', 'Twista', 2, 1000, NULL, 17
 INSERT INTO public.hat_defs VALUES (1526, 'Fire II', 'HOT HOT HOT!', 2, 1000, NULL, 1711678914);
 INSERT INTO public.hat_defs VALUES (1537, 'greeble', ':greeble:', 2, 500, NULL, 1713150123);
 
+
 INSERT INTO public.hat_defs VALUES (1396, 'Click Me', 'Come on, you know you want to see my profile.', 2, 500, NULL, 1704921421);
 INSERT INTO public.hat_defs VALUES (1408, 'Kadio Stance', '*thwack* YEEEEEEEEOUCHHHHHHHHHH', 2, 500, NULL, 1705616850);
 INSERT INTO public.hat_defs VALUES (1381, 'Griffith Helmet', 'The Falcon has fallen. Billions must die.', 2, 500, NULL, 1698690002);
@@ -1266,7 +1267,7 @@ INSERT INTO public.hat_defs VALUES (1534, 'Kitty Pawz', 'Kawaii? Sure. But deep 
 -- Name: hat_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hat_defs_id_seq', 1539, true);
+SELECT pg_catalog.setval('public.hat_defs_id_seq', 1540, true);
 
 
 --
