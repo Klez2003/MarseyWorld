337e243e9f558fe2ebdc4e358f7fb800c86b21ab
Aevann <randomname42029@gmail.com>
2024-04-26 23:38:29 +0300

    Revert "remove unnecessary function"
    This reverts commit 73649e987c7440ace3567607ae94edab4c077e72.

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -985,7 +985,7 @@ forced_hats = {
 	"sharpen": ("Bane Mask", "No one understands..."),
 	"earlylife": ("The Merchant", "SHUT IT DOWN, the goys know!"),
 	"marsify": ("Marsified", "I can't pick my own Marseys, help!"),
-	"is_banned": ("Behind Bars", "This user is banned and needs to do better!"),
+	"is_suspended": ("Behind Bars", "This user is banned and needs to do better!"),
 	"chud": (
 				("Egg_irl", "This user is getting in touch with xir identity!"),
 				("Trans Flag", "Just in case you forgot, trans lives matter."),
