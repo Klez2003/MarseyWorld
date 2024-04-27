cc0485f45e902142a9b09b2505b01115668e8177
Aevann <randomname42029@gmail.com>
2024-04-28 00:00:14 +0300

    make confetti tied to award and make it more cancerous

---
+++ files/assets/js/click_confetti.js
@@ -0,0 +1,20 @@
+document.addEventListener('click', function(e) {
+	function random(max){
+		return Math.random() * (max - 0) + 0;
+	}
+
+	const c = document.createElement('div');
+	c.classList.add('click-confetti')
+	c.style.top = e.clientY + 'px';
+	c.style.left = e.clientX + 'px';
+	for (let i = 0; i < 100; i++) {		
+		const e = document.createElement("i");
+		e.style.transform = 'translate3d(' + (random(500) - 250) + 'px, ' + (random(200) - 150) + 'px, 0) rotate(' + random(360) + 'deg)'
+		e.style.background = `hsla(${random(360)},100%,50%,1)`
+		c.appendChild(e);
+	}
+	document.body.appendChild(c);
+	setTimeout(function() {
+		document.body.removeChild(c)
+	}, 700)
+})
