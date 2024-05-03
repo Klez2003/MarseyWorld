3325ba56b99dc85e363e61100bdb7eb8e60cd7a2
Aevann <randomname42029@gmail.com>
2024-05-03 10:35:30 +0300

    hide rapist

--- files/helpers/can_see.py
+++ files/helpers/can_see.py
@@ -5,7 +5,7 @@ from files.classes.hole import Hole
 from flask import request
 
 #DELETE_ME_PLS
-words_to_hide = ('israel', 'isreal', 'palest', 'muslim', 'islam', 'hamas', 'jew', 'zion', 'gaza', 'rafah', 'isis', 'terror', 'iraq', 'allah', 'mohammad', 'muhammad', 'mohammed', 'muhammed', 'mohamad', 'muhamad', 'mohamed', 'muhamed', 'trans', 'train', 'tranny', 'troon', 'rowdy', 'nigger', 'bipoc', 'rape')
+words_to_hide = ('israel', 'isreal', 'palest', 'muslim', 'islam', 'hamas', 'jew', 'zion', 'gaza', 'rafah', 'isis', 'terror', 'iraq', 'allah', 'mohammad', 'muhammad', 'mohammed', 'muhammed', 'mohamad', 'muhamad', 'mohamed', 'muhamed', 'trans', 'train', 'tranny', 'troon', 'rowdy', 'nigger', 'bipoc', 'rape', 'rapist')
 
 def can_see(user, obj):
 	if isinstance(obj, (Post, Comment)):
