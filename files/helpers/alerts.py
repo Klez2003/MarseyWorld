3473f1ee1084ac5dd74299ac0a8f2df9e59c4fe0
Aevann <randomname42029@gmail.com>
2024-05-22 19:02:30 +0300

    fix 500 error

--- files/helpers/alerts.py
+++ files/helpers/alerts.py
@@ -257,7 +257,9 @@ def NOTIFY_USERS(text, v, oldtext=None, ghost=False, obj=None, followers_ping=Tr
 			if coin_receivers:
 				g.db.query(User).options(load_only(User.id)).filter(User.id.in_(coin_receivers)).update({ User.coins: User.coins + 5 })
 
-	largest_ping_group_count = g.db.query(func.count(GroupMembership.group_name)).group_by(GroupMembership.group_name).order_by(func.count(GroupMembership.group_name).desc()).first()[0]
+	largest_ping_group_count = g.db.query(func.count(GroupMembership.group_name)).group_by(GroupMembership.group_name).order_by(func.count(GroupMembership.group_name).desc()).first()
+	if largest_ping_group_count: largest_ping_group_count = largest_ping_group_count[0]
+	else: largest_ping_group_count = 0
 	max_ping_count = max(400, largest_ping_group_count)
 	if len(notify_users) > max_ping_count and v.admin_level < PERMS['POST_COMMENT_INFINITE_PINGS']:
 		abort(403, f"You can only notify a maximum of {max_ping_count} users.")
