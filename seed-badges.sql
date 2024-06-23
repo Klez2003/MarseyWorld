28930d95bfd926b84625567fa86d260f291847a3
SneedBot <rdrama@rdrama.net>
2024-06-23 12:00:17 +0000

    sneed

--- seed-badges.sql
+++ seed-badges.sql
@@ -46,6 +46,7 @@ INSERT INTO public.badge_defs VALUES (342, '3rd Birthgay Bash Survivor', 'Awarde
 INSERT INTO public.badge_defs VALUES (183, '2022 Spooooooky Marsey Artist', 'Contributed a VERY SCARY Marsey for Halloween 2022!', 1666225718);
 INSERT INTO public.badge_defs VALUES (344, 'Summer of PRIDE Celebrator', 'Ensured GloboHomo World Domination during the Summer of Pride!', 1717954486);
 INSERT INTO public.badge_defs VALUES (345, 'Cucked', 'This user has watched the love of his life get railed by another man 😔😢', 1718787648);
+INSERT INTO public.badge_defs VALUES (346, 'Marsey Tattoo', 'This user permanently supports Marsey, and the Zune. 😉', 1719132081);
 INSERT INTO public.badge_defs VALUES (176, 'Fish Of The Year', 'Awarded for outstanding achievement in the field of achievement. Well done!', 1663340894);
 INSERT INTO public.badge_defs VALUES (177, 'Sweatshop Laborer', 'lmao thanks retard', 1663616592);
 INSERT INTO public.badge_defs VALUES (178, 'Marseyhypebeast', 'This user owns OFFICIAL MARSEY MERCH and has waaaaay more sex than you.', 1663616913);
@@ -308,7 +309,7 @@ INSERT INTO public.badge_defs VALUES (73, 'Le Rich Gentlesir', 'Spent a fucking 
 -- Name: badge_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.badge_defs_id_seq', 345, true);
+SELECT pg_catalog.setval('public.badge_defs_id_seq', 346, true);
 
 
 --
