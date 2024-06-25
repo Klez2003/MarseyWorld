4dd90e8ac6d325fba72de88381a59441b5c3b6b0
Aevann <randomname42029@gmail.com>
2024-06-25 23:29:55 +0300

    no dash

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -1471,7 +1471,7 @@ def pin_post(post_id, v):
 			send_repeatable_notification(post.author_id, f"@{v.username} (a site admin) has pinned {post.textlink}")
 	else:
 		if permapinned >= PIN_LIMIT:
-			abort(403, f"Can't have more than {PIN_LIMIT} perma-pinned posts!")
+			abort(403, f"Can't have more than {PIN_LIMIT} permapinned posts!")
 		post.pinned_utc = None
 		pin_time = 'permanently'
 		code = 201
