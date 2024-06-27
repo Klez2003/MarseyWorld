d52abf8fd480cf491d8d3a9d23676b395bd332ce
Aevann <randomname42029@gmail.com>
2024-06-27 14:04:38 +0300

    fix this https://rdrama.net/post/18459/marseycapywalking-megathread-for-bugs-and-suggestions/6586016#context

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -1036,7 +1036,7 @@ forced_hats = {
 
 IMAGE_FORMATS = ('png','jpg','jpeg','webp','gif')
 VIDEO_FORMATS = ('mp4','webm','mov','avi','mkv','flv','m4v','3gp')
-AUDIO_FORMATS = ('mp3','wav','ogg','aac','m4a','flac')
+AUDIO_FORMATS = ('mp3','wav','ogg','aac','m4a','flac','oga')
 
 if not IS_LOCALHOST and SECRET_KEY == DEFAULT_CONFIG_VALUE:
 	from warnings import warn
