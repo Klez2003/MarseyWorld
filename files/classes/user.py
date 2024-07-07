dc9577dc4068c8ba63f12ecc0d82fb661f9dfa3f
Aevann <randomname42029@gmail.com>
2024-07-07 18:41:38 +0300

    exclude yet another chat

--- files/classes/user.py
+++ files/classes/user.py
@@ -857,7 +857,7 @@ class User(Base):
 			return g.db.query(ChatMembership).filter(
 				ChatMembership.user_id == self.id,
 				ChatMembership.notification == True,
-				ChatMembership.chat_id.notin_((12,219,320,360,379,395,427,448)),
+				ChatMembership.chat_id.notin_((12,219,320,360,379,395,427,448,454)),
 			).count()
 		return g.db.query(ChatMembership).filter_by(user_id=self.id, notification=True).count()
 
