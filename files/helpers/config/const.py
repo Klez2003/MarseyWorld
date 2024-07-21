6cb4c2f66b469812377d152407635a81429b92a1
Aevann <randomname42029@gmail.com>
2024-07-21 20:16:33 +0300

    add longpostbot quote

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -405,6 +405,7 @@ LONGPOSTBOT_REPLIES = (
 	"> Blah Blah blah blah\n\n> yap yap yap yap\n\nDidnt read",
 	"https://i.rdrama.net/images/17194996128531878.webp",
 	"what is bro yapping about :marseyskull:",
+	"https://i.rdrama.net/images/17215821618015614.webp",
 )
 
 CHUD_MSGS = (
