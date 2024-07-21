98295b4c0004800dc81a09d50bcad1a55923c9df
Aevann <randomname42029@gmail.com>
2024-07-21 17:00:12 +0300

    nerf comment spam filter

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -13,8 +13,8 @@ SPAM_URL_SIMILARITY_THRESHOLD = 0.1
 SPAM_SIMILARITY_THRESHOLD = 0.5
 SPAM_SIMILAR_COUNT_THRESHOLD = 10
 
-COMMENT_SPAM_SIMILAR_THRESHOLD = 0.5
-COMMENT_SPAM_COUNT_THRESHOLD = 10
+COMMENT_SPAM_SIMILAR_THRESHOLD = 0.2
+COMMENT_SPAM_COUNT_THRESHOLD = 40
 
 DISABLE_DOWNVOTES = False
 DESCRIPTION = "rdrama.net caters to drama in all forms such as: Real life, videos, photos, gossip, rumors, news sites, Reddit, and Beyond™. There isn't drama we won't touch, and we want it all!"
