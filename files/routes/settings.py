d405982dd7dc0853b9e876f9248bbd28fc73d846
Aevann <randomname42029@gmail.com>
2024-07-22 23:05:55 +0300

    less sensitive pronoun slur filter for fishyman

--- files/routes/settings.py
+++ files/routes/settings.py
@@ -1047,8 +1047,10 @@ def settings_pronouns_change(v):
 		abort(400, "The pronouns you entered don't match the required format!")
 
 	bare_pronouns = pronouns.lower().replace('/', '')
-	if 'nig' in bare_pronouns: pronouns = 'BI/POC'
-	elif 'fag' in bare_pronouns: pronouns = 'cute/twink'
+	if bare_pronouns.startswith('nig'):
+		pronouns = 'BI/POC'
+	elif bare_pronouns.startswith('fag'):
+		pronouns = 'cute/twink'
 
 	v.pronouns = pronouns
 	g.db.add(v)
