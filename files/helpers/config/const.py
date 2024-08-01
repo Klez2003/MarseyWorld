b3b35a96aa183ab85aa8af873a4ee99c44a3e1c3
Aevann <randomname42029@gmail.com>
2024-08-01 19:43:13 +0300

    add longpostbot reply

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -462,6 +462,7 @@ LONGPOSTBOT_REPLIES = (
 	"https://i.rdrama.net/images/17224404628678622.webp",
 	"Praying Trump gets elected so you get sent to some kind of concentration camp",
 	"Didn't read",
+	"?",
 )
 
 CHUD_MSGS = (
