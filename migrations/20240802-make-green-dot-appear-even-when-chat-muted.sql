31b62d753e3913e0dab58a94e68973d9feec51ac
Aevann <randomname42029@gmail.com>
2024-08-02 11:42:55 +0300

    fix migration file

--- migrations/20240802-make-green-dot-appear-even-when-chat-muted.sql
+++ migrations/20240802-make-green-dot-appear-even-when-chat-muted.sql
@@ -1,4 +1,4 @@
 alter table chat_memberships add column muted bool not null default false;
 alter table chat_memberships alter column muted drop default;
-update chat_memberships set muted=true, notification=false where notification=null;
+update chat_memberships set muted=true, notification=false where notification is null;
 alter table chat_memberships alter column notification set not null;
