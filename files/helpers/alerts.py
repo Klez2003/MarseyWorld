7946ea4ab2e3f8c9a9f10d01b22c13323f95bbdc
Aevann <randomname42029@gmail.com>
2024-08-06 20:43:23 +0300

    increase ping limit from 400 to 1000

--- files/helpers/alerts.py
+++ files/helpers/alerts.py
@@ -269,7 +269,7 @@ def NOTIFY_USERS(text, v, oldtext=None, ghost=False, obj=None, followers_ping=Tr
 	largest_ping_group_count = g.db.query(func.count(GroupMembership.group_name)).group_by(GroupMembership.group_name).order_by(func.count(GroupMembership.group_name).desc()).first()
 	if largest_ping_group_count: largest_ping_group_count = largest_ping_group_count[0]
 	else: largest_ping_group_count = 0
-	max_ping_count = max(400, largest_ping_group_count)
+	max_ping_count = max(1000, largest_ping_group_count)
 	if len(notify_users) > max_ping_count and v.admin_level < PERMS['POST_COMMENT_INFINITE_PINGS']:
 		abort(403, f"You can only notify a maximum of {max_ping_count} users.")
 
