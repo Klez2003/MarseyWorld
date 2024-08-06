522ab4867d04d6866a761cad1316a3cd417d9fe9
Aevann <randomname42029@gmail.com>
2024-08-06 19:17:54 +0300

    diverge wpd and rdrama perms

--- files/helpers/cron.py
+++ files/helpers/cron.py
@@ -386,6 +386,8 @@ def _unpin_expired():
 def _give_marseybux_salary():
 	for u in g.db.query(User).filter(User.admin_level > 0).all():
 		marseybux_salary = u.admin_level * 10000
+		if SITE_NAME == 'WPD':
+			marseybux_salary *= 2
 		u.pay_account('marseybux', marseybux_salary, "Janny salary")
 		send_repeatable_notification(u.id, f"You have received your monthly janny salary of {marseybux_salary} Marseybux!")
 
