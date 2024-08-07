a58c071c1dbd4581856435842fda623eb2182da1
Aevann <randomname42029@gmail.com>
2024-08-07 06:12:31 +0300

    fix this https://rdrama.net/h/meta/post/290396/patsy-is-currently-begging-for-his/6816690#context

--- files/assets/js/emoji_modal/inline_emoji_modal.js
+++ files/assets/js/emoji_modal/inline_emoji_modal.js
@@ -199,9 +199,9 @@ function update_inline_emoji_modal(event)
 			const found = globalGroups.filter(i => resultSet.has(i));
 
 			if (found.length == 1 && found[0].toLowerCase() == current_word.substring(1))
-				populate_inline_user_modal([], event.target);
+				populate_inline_group_modal([], event.target);
 			else
-				populate_inline_user_modal(found, event.target);
+				populate_inline_group_modal(found, event.target);
 		});
 	}
 	else if (current_word && curr_word_is_user() && current_word != "@")
