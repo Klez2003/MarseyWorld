ddc4c24f1fb73b30662ad8867e3cc2aba7f07616
Aevann <randomname42029@gmail.com>
2024-08-09 23:57:21 +0300

    improve last commit

--- files/assets/js/comments_v.js
+++ files/assets/js/comments_v.js
@@ -351,14 +351,16 @@ function post_comment(fullname, hide) {
 					viewbtn.dataset.ids = viewbtn.dataset.ids.slice(0, -1) + `, ${data['id']}]`
 			}
 
-			if (comment.includes('<blockquote class="twitter-tweet">') && !document.getElementById('twitter-embed-script')) {
-				const script = document.createElement('script');
-				script.id = 'twitter-embed-script'
-				script.src = '/assets/js/vendor/twitter.js?x=11';
-				script.defer = true;
-				comments.insertAdjacentElement('beforeEnd', script);
+			if (comment.includes('<blockquote class="twitter-tweet">')) {
+				if (!document.getElementById('twitter-embed-script')) {
+					const script = document.createElement('script');
+					script.id = 'twitter-embed-script'
+					script.src = '/assets/js/vendor/twitter.js?x=11';
+					script.defer = true;
+					comments.insertAdjacentElement('beforeEnd', script);
+				}
+				embed_twitter()
 			}
-			embed_twitter()
 		}
 		else {
 			showToast(false, getMessageFromJsonData(false, data));
