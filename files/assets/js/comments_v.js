6ef16f5477a2b8609d1c17748b948052b3ec320c
Aevann <randomname42029@gmail.com>
2024-08-09 23:52:17 +0300

    fix embedding twitter on posting comment

--- files/assets/js/comments_v.js
+++ files/assets/js/comments_v.js
@@ -351,6 +351,13 @@ function post_comment(fullname, hide) {
 					viewbtn.dataset.ids = viewbtn.dataset.ids.slice(0, -1) + `, ${data['id']}]`
 			}
 
+			if (!document.getElementById('twitter-embed-script')) {
+				const script = document.createElement('script');
+				script.id = 'twitter-embed-script'
+				script.src = '/assets/js/vendor/twitter.js?x=11';
+				script.defer = true;
+				comments.insertAdjacentElement('beforeEnd', script);
+			}
 			embed_twitter()
 		}
 		else {
