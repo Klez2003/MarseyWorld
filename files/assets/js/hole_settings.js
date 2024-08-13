897fc6386f627641e9e77ea8e4a48fe176b3ecc1
Aevann <randomname42029@gmail.com>
2024-08-13 23:51:03 +0300

    expandable and naviable banners and sidebars in hole settings

--- files/assets/js/hole_settings.js
+++ files/assets/js/hole_settings.js
@@ -9,7 +9,7 @@ function upload_banner(t, hole_name) {
 			const url = JSON.parse(xhr.response)['url']
 			const html = `
 			<section class="mt-5 d-block hole-settings-subsection">
-				<img class="mr-3" loading="lazy" alt="/h/${hole_name} banner" src="${url}">
+				<img class="mr-3 img" loading="lazy" alt="/h/${hole_name} banner" src="${url}">
 				<button class="btn btn-danger hole-banner-delete-button mt-2" data-nonce="${nonce}" data-onclick="areyousure(this)" data-areyousure="delete_image(this, '/h/${hole_name}/settings/banners/delete')">Delete</button>
 			</section>`
 
@@ -33,7 +33,7 @@ function upload_sidebar(t, hole_name) {
 			const url = JSON.parse(xhr.response)['url']
 			const html = `
 			<section class="mt-5 d-block hole-settings-subsection">
-				<img class="mr-3" loading="lazy" alt="/h/${hole_name} sidebar" src="${url}">
+				<img class="mr-3 img" loading="lazy" alt="/h/${hole_name} sidebar" src="${url}">
 				<button class="btn btn-danger hole-sidebar-delete-button mt-2" data-nonce="${nonce}" data-onclick="areyousure(this)" data-areyousure="delete_image(this, '/h/${hole_name}/settings/sidebars/delete')">Delete</button>
 			</section>`
 
