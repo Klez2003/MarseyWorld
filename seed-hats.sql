e9a6386ef7c67581afd53ebc42710c9302055244
SneedBot <rdrama@rdrama.net>
2024-08-14 08:00:17 +0000

    sneed

--- seed-hats.sql
+++ seed-hats.sql
@@ -1176,6 +1176,7 @@ INSERT INTO public.hat_defs VALUES (1617, 'Taco', 'gracias', 2, 500, NULL, 17198
 INSERT INTO public.hat_defs VALUES (1613, 'Frost', 'Feel the chills', 2, 500, NULL, 1719818291);
 
 INSERT INTO public.hat_defs VALUES (1650, 'Janny Broom', 'I LOVE doing it for free', 2, 1000, NULL, 1723383395);
+
 INSERT INTO public.hat_defs VALUES (1450, 'Red Eminent Horns', 'Queen of the devils, leader of the chuds.', 2, 500, NULL, 1708367282);
 INSERT INTO public.hat_defs VALUES (1544, 'Sweden Flag', 'Över Norden jag härskar, med det arv som jag gavs, gång på gång, sjung Carolus sång!', 2, 1000, NULL, 1714318091);
 INSERT INTO public.hat_defs VALUES (1377, 'pkmn0094', 'Gengar, the Shadow Pokémon. A Ghost and Poison type. Gengar can pass through dimensions and appear almost anywhere. Since Gengar absorbs any warmth around it, the temperature suddenly drops wherever it appears.', 2, 500, NULL, 1698658745);
@@ -1301,7 +1302,7 @@ INSERT INTO public.hat_defs VALUES (1534, 'Kitty Pawz', 'Kawaii? Sure. But deep 
 -- Name: hat_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hat_defs_id_seq', 1650, true);
+SELECT pg_catalog.setval('public.hat_defs_id_seq', 1652, true);
 
 
 --
