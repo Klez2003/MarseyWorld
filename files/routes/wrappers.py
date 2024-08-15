c6983757de6894e33d01dcacf72661f69ecbfa3d
Aevann <aevann@noreply.fsdfsd.net>
2024-08-15 21:47:54 +0000

    Update files/routes/wrappers.py

--- files/routes/wrappers.py
+++ files/routes/wrappers.py
@@ -57,7 +57,7 @@ def calc_users():
 		g.loggedout_counter = len(loggedout)
 
 		if SITE == 'watchpeopledie.tv':
-			ddos_threshold = 3500
+			ddos_threshold = 4000
 		else:
 			ddos_threshold = 2000
 
