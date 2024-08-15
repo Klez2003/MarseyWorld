8cdc51a1973afb66d1905d7614f83d9a1ccd9489
Aevann <randomname42029@gmail.com>
2024-08-15 16:44:34 +0300

    remove unnecessary

--- files/classes/post.py
+++ files/classes/post.py
@@ -453,7 +453,7 @@ class Post(Base):
 			return False
 
 		soup = BeautifulSoup(self.body_html, 'lxml')
-		tags = soup.html.body.find_all(lambda tag: tag.name in {'p','ul','li','table'} and tag.text, recursive=False)
+		tags = soup.html.body.find_all(lambda tag: tag.name in {'p','ul','table'} and tag.text, recursive=False)
 		post_char_count = 0
 		for tag in tags:
 			post_char_count += len(tag.text)
