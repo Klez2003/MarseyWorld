0c53c1f71b2327d77ded5e4fff428507cd562ddf
Aevann <randomname42029@gmail.com>
2024-08-17 19:11:44 +0300

    dont embed reports and notifications about reports

--- files/assets/js/reddit_embed.js
+++ files/assets/js/reddit_embed.js
@@ -3,7 +3,7 @@ function embed_reddit() {
 
 	for (const a of document.querySelectorAll('a[href^="https://old.reddit.com/r/"]:not(a[href$="/new"])')) {
 		if (a.innerHTML && a.innerHTML !== a.href) continue
-		if (a.parentElement.tagName == "STRONG") continue
+		if (["STRONG", "LI", "BLOCKQUOTE"].includes(a.parentElement.tagName)) continue
 
 		let iframe_src = a.href.replace('https://old.reddit.com/', 'https://embed.reddit.com/')
 		iframe_src = iframe_src.split('?')[0]
