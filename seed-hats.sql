a6a31da4d35e6948b8645c73bc9ae46fd116911d
SneedBot <rdrama@rdrama.net>
2024-09-14 20:00:22 +0000

    sneed

--- seed-hats.sql
+++ seed-hats.sql
@@ -1233,6 +1233,7 @@ INSERT INTO public.hat_defs VALUES (1681, 'Mysterious Challenger', '...', 2, 500
 INSERT INTO public.hat_defs VALUES (1683, 'Fathers no evil', '"A lot of splendid high profile court cases could trace it."', 2, 500, NULL, 1725809328);
 INSERT INTO public.hat_defs VALUES (1686, 'upright JUUL', 'zoomercore non-acoustic vaping device upright.', 2, 500, NULL, 1725910576);
 
+
 INSERT INTO public.hat_defs VALUES (1396, 'Click Me', 'Come on, you know you want to see my profile.', 2, 500, NULL, 1704921421);
 INSERT INTO public.hat_defs VALUES (1408, 'Kadio Stance', '*thwack* YEEEEEEEEOUCHHHHHHHHHH', 2, 500, NULL, 1705616850);
 INSERT INTO public.hat_defs VALUES (1381, 'Griffith Helmet', 'The Falcon has fallen. Billions must die.', 2, 500, NULL, 1698690002);
@@ -1313,7 +1314,7 @@ INSERT INTO public.hat_defs VALUES (1534, 'Kitty Pawz', 'Kawaii? Sure. But deep 
 -- Name: hat_defs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.hat_defs_id_seq', 1689, true);
+SELECT pg_catalog.setval('public.hat_defs_id_seq', 1690, true);
 
 
 --
