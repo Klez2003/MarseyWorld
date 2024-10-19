a7e9bf9f2494ebe4575c375163167ac9bfd87668
Aevann <randomname42029@gmail.com>
2024-10-19 06:48:38 +0300

    fix prev commit

--- migrations/20241019-increase-rendered-ban-reason-charlimit.sql
+++ migrations/20241019-increase-rendered-ban-reason-charlimit.sql
@@ -1,2 +1,3 @@
 alter table users alter column ban_reason type varchar(5000);
 alter table users alter column shadowban_reason type varchar(5000);
+alter table modactions alter column _note type varchar(5050);
