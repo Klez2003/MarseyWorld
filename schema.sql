2d458bfa904f4e9237410b286fd4c2c913faa4d8
Aevann <randomname42029@gmail.com>
2024-10-25 00:33:35 +0300

    sneed

--- schema.sql
+++ schema.sql
@@ -782,7 +782,8 @@ CREATE TABLE public.media (
     created_utc integer NOT NULL,
     size integer NOT NULL,
     posterurl character varying(65),
-    referrer character varying(200)
+    referrer character varying(200),
+    purged_utc integer
 );
 
 
