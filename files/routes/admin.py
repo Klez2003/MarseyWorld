b853ad04f4f2657c6bd94368baf8f90dada39b06
Aevann <randomname42029@gmail.com>
2024-10-26 17:49:26 +0300

    same as last commit

--- files/routes/admin.py
+++ files/routes/admin.py
@@ -1917,10 +1917,9 @@ def delete_media_post(v):
 	g.db.add(media)
 
 	if url.startswith(SITE_FULL_VIDEOS):
-		posterurl = media.posterurl
-		if posterurl:
-			remove_image_using_link(posterurl[0])
-			purge_files_in_cloudflare_cache(posterurl[0])
+		if media.posterurl:
+			remove_image_using_link(media.posterurl)
+			purge_files_in_cloudflare_cache(media.posterurl)
 
 	ma = ModAction(
 		kind="delete_media",
