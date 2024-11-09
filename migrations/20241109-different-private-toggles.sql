d075787c7f68c7e7b12677236bea717c46fbbad1
Aevann <randomname42029@gmail.com>
2024-11-09 18:27:13 +0200

    shorter

--- migrations/20241109-different-private-toggles.sql
+++ migrations/20241109-different-private-toggles.sql
@@ -1,6 +1,6 @@
 alter table users add column private_posts bool DEFAULT false NOT NULL;
 alter table users add column private_comments bool DEFAULT false NOT NULL;
-update users set private_posts=is_private, private_comments=is_private;
+update users set private_posts=true, private_comments=true where is_private=true;
 
 create index user_private_posts_idx ON public.users USING btree (private_posts);
 create index user_private_comments_idx ON public.users USING btree (private_comments);
