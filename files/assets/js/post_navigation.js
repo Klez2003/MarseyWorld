e8d290cdee927a35690dcd51fc0ffaa109ab45df
Aevann <randomname42029@gmail.com>
2024-11-09 11:58:57 +0200

    full support for keyboard nav

--- files/assets/js/post_navigation.js
+++ files/assets/js/post_navigation.js
@@ -4,6 +4,8 @@ if (post_permalinks) {
 	post_permalinks = post_permalinks.split(', ');
 	let current_index = post_permalinks.indexOf(`'${location.href}'`)
 	if (current_index > -1) {
+		set_href(sessionStorage.getItem("next_page_url") )
+
 		let permalink_after = post_permalinks[current_index+1]
 
 		if (permalink_after) {
@@ -11,41 +13,33 @@ if (post_permalinks) {
 			while (comments[pid]) {
 				current_index += 1
 				permalink_after = post_permalinks[current_index]
-				if (permalink_after) {
-					pid = permalink_after.split('/').slice(-2, -1)[0]
-				}
-				else {
-					permalink_after = '+' + sessionStorage.getItem("next_page_url") + '+'
-					break
-				}
+				pid = permalink_after.split('/').slice(-2, -1)[0]
 			}
 
-			for (const btn of document.getElementsByClassName('post_navigation')) {
-				btn.classList.remove('d-none')
-				btn.href = permalink_after.slice(1, -1)
-			}
-
-			document.addEventListener('keydown', (e) => {
-				if (
-					!["TEXTAREA", "INPUT"].includes(document.activeElement.tagName) &&
-					!(e.ctrlKey || e.metaKey || e.shiftKey || e.altKey) &&
-					!expandImageModal.classList.contains('show')
-				) {
-					for (const video of document.getElementsByTagName('video'))
-						if (!video.paused) return
-
-					if (["ArrowRight", "d"].includes(e.key))
-						location.href = permalink_after.slice(1, -1)
-					if (["ArrowLeft", "a"].includes(e.key))
-						history.back()
-				}
-			})
-		}
-		else {
-			for (const btn of document.getElementsByClassName('post_navigation')) {
-				btn.classList.remove('d-none')
-				btn.href = sessionStorage.getItem("next_page_url")
-			}
+			set_href(permalink_after.slice(1, -1))
 		}
 	}
+}
+
+function set_href(href) {
+	for (const btn of document.getElementsByClassName('post_navigation')) {
+		btn.classList.remove('d-none')
+		btn.href = href
+	}
+
+	document.addEventListener('keydown', (e) => {
+		if (
+			!["TEXTAREA", "INPUT"].includes(document.activeElement.tagName) &&
+			!(e.ctrlKey || e.metaKey || e.shiftKey || e.altKey) &&
+			!expandImageModal.classList.contains('show')
+		) {
+			for (const video of document.getElementsByTagName('video'))
+				if (!video.paused) return
+
+			if (["ArrowRight", "d"].includes(e.key))
+				location.href = href
+			if (["ArrowLeft", "a"].includes(e.key))
+				history.back()
+		}
+	})
 }
