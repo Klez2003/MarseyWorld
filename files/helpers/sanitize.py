d4b34ce5ad36b1c07b9efacdc319e8923fc83ebc
Aevann <randomname42029@gmail.com>
2024-11-11 21:09:49 +0200

    fix this https://rdrama.net/h/braincels/post/298767/marseyelliotrodger3-if-you-havent-spent-four

--- files/helpers/sanitize.py
+++ files/helpers/sanitize.py
@@ -731,6 +731,9 @@ def is_whitelisted(domain, k):
 	if domain not in {'youtube.com','reddit.com','x.com','msn.com','wsj.com','tiktok.com','forbes.com','dailymail.co.uk','facebook.com','spotify.com','nytimes.com','businessinsider.com','instagram.com','yahoo.com','thedailybeast.com','nypost.com','newsweek.com','bloomberg.com','quora.com','nbcnews.com','reuters.com','tmz.com','cnbc.com','marketwatch.com','thetimes.co.uk','sfchronicle.com','washingtonpost.com','cbsnews.com','foxnews.com','bbc.com','bbc.co.uk','ifunny.co','independent.co.uk','wikipedia.org','substack.com','google.com'}:
 		return True
 
+	if domain == 'nytimes.com' and k == 'unlocked_article_code':
+		return True
+
 	if domain == 'wikipedia.org' and k != 'wprov':
 		return True
 
