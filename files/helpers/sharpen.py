e4a84852dd42a223de66522bf6b26196360b66d7
Aevann <randomname42029@gmail.com>
2024-11-12 21:34:23 +0200

    fix the space

--- files/helpers/sharpen.py
+++ files/helpers/sharpen.py
@@ -5,7 +5,7 @@ def sharpen(string, chud_phrase):
 	if chud_phrase:
 		string = re.sub(chud_phrase, 'erfdsx34224e4535resfed', string, flags=re.I)
 
-	string = the_fucking_regex.sub("\g<2> fucking", string)
+	string = the_fucking_regex.sub("\g<1><2> fucking", string)
 	string = bitch_question_mark_regex.sub(", bitch?", string)
 	string = exclamation_point_regex.sub(", motherfucker!", string)
 
