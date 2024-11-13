b85cf708e8edd62f5b4e4d1d0b285ea1a70bb081
Aevann <randomname42029@gmail.com>
2024-11-14 01:26:36 +0200

    remove print statements

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -697,9 +697,7 @@ def submit_post(v, hole=None):
 			stop(415)
 	elif p.url and p.url.startswith(SITE_FULL_VIDEOS):
 		filename = '/videos' + p.url.split(SITE_FULL_VIDEOS)[1]
-		print(filename, flush=True)
 		media = g.db.get(Media, filename)
-		print(media, flush=True)
 		if media:
 			media_usage = MediaUsage(
 				filename=filename,
