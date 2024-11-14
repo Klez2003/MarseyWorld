3c9152392540dcef592a3eb598f41d094c7a809c
Aevann <randomname42029@gmail.com>
2024-11-14 02:03:31 +0200

    remove cancer that complicates code and causes bugs

--- files/routes/posts.py
+++ files/routes/posts.py
@@ -604,9 +604,6 @@ def submit_post(v, hole=None):
 	if embed and len(embed) > 1500: embed = None
 	if embed: embed = embed.strip()
 
-	if url and url.startswith(f'{SITE_FULL}/'):
-		url = url.split(SITE_FULL)[1]
-
 	if url == '': url = None
 
 	p = Post(
