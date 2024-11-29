97522eb30dd10a46041ef97566f02f3a8f77afff
Aevann <randomname42029@gmail.com>
2024-11-29 21:10:10 +0200

    do this https://rdrama.net/h/meta/post/321507/request-make-the-site-compatible-with

--- files/helpers/config/const.py
+++ files/helpers/config/const.py
@@ -1328,7 +1328,7 @@ forced_hats = {
 
 }
 
-IMAGE_FORMATS = ('png','jpg','jpeg','webp','gif')
+IMAGE_FORMATS = ('png','jpg','jpeg','webp','gif','avif')
 VIDEO_FORMATS = ('mp4','webm','mov','avi','mkv','flv','m4v','3gp')
 AUDIO_FORMATS = ('mp3','wav','ogg','aac','m4a','flac','oga')
 
