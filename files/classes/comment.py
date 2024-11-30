e51139f20d403167a4f25c41975d63eb52d5df0a
Aevann <randomname42029@gmail.com>
2024-11-30 23:04:55 +0200

    remove now-unused

--- files/classes/comment.py
+++ files/classes/comment.py
@@ -440,7 +440,7 @@ class Comment(Base):
 		return False
 
 	@lazy
-	def realbody(self, v, community_notes=True):
+	def realbody(self, v):
 		if self.deleted_utc != 0 and not (v and (v.admin_level >= PERMS['POST_COMMENT_MODERATION'] or v.id == self.author.id)):
 			return "[Deleted by user]"
 		if self.is_banned and not (v and v.admin_level >= PERMS['POST_COMMENT_MODERATION']) and not (v and v.id == self.author.id):
